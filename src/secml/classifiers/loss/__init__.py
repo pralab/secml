4c374eb620f65ac8ba3cd881e69898506b214583
mmelis <marco.melis@diee.unica.it>
2018-09-14 19:47:37 +0200

    Added loss softmax and loss logistic to init.

--- src/secml/classifiers/loss/__init__.py
+++ src/secml/classifiers/loss/__init__.py
@@ -2,3 +2,5 @@ from c_loss import CLoss
 from c_loss_epsilon_insensitive import CLossEpsilonInsensitive, CLossSquaredEpsilonInsensitive
 from c_loss_hinge import CLossHinge, CLossSquaredHinge
 from c_loss_squared import CLossSquared
+from c_loss_softmax import CLossSoftMax
+from c_loss_logistic import CLossLogistic
