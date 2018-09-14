288c569b9b60a35b0c6af86073591fae415672ac
mmelis <marco.melis@diee.unica.it>
2018-09-14 15:33:39 +0200

    Fixed import of figure package by adding new plot types.
    (Backported from aisec18)

--- src/secml/figure/plots/__init__.py
+++ src/secml/figure/plots/__init__.py
@@ -2,3 +2,5 @@ from c_plot import CPlot
 from c_plot_roc import CPlotRoc
 from c_plot_fun import CPlotFunction
 from c_plot_ds import CPlotDataset
+from c_plot_conf_matr import CPlotConfMatr
+from c_plot_sec_eval import CPlotSecEval
