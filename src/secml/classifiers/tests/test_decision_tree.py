f36592032190d3fa1bea3bb58a7afd6e8a4f1312
Marco Melis <marco.melis@diee.unica.it>
2018-10-05 16:13:26 +0200

    Updated and improved unittests for more classifiers.
    - CClassifierDecisionTree
    - CClassifierKNN
    - CClassifierRandomForest

--- src/secml/classifiers/tests/test_decision_tree.py
+++ src/secml/classifiers/tests/test_decision_tree.py
@@ -1,18 +1,11 @@
-"""
-Created on 27/apr/2015
-Class to test CDecisionTree
-
-@author: Davide Maiorca
-If you find any BUG, please notify authors first.
-"""
-import unittest
 from secml.utils import CUnitTest
 
 from secml.data.loader import CDLRandomToy
 from secml.classifiers import CClassifierDecisionTree
+from secml.array import CArray
 
 
-class TestCDecisionTree(CUnitTest):
+class TestCClassifierDecisionTree(CUnitTest):
     """Unit test for CDecisionTree."""
 
     def setUp(self):
@@ -48,6 +41,154 @@ class TestCDecisionTree(CUnitTest):
         self.logger.info("Class of affinity: {:}".format(y))
         self.assertEquals(y, self.dataset.Y[120], "Wrong classification")
 
+    def test_fun(self):
+        """Test for discriminant_function() and classify() methods."""
+        self.logger.info(
+            "Test for discriminant_function() and classify() methods.")
+
+        def _check_df_scores(s, n_samples):
+            self.assertEqual(type(s), CArray)
+            self.assertTrue(s.isdense)
+            self.assertEqual(1, s.ndim)
+            self.assertEqual((n_samples,), df_scores_0.shape)
+            self.assertEqual(df_scores_0.dtype, float)
+
+        def _check_classify_scores(l, s, n_samples, n_classes):
+            self.assertEqual(type(l), CArray)
+            self.assertEqual(type(s), CArray)
+            self.assertTrue(l.isdense)
+            self.assertTrue(s.isdense)
+            self.assertEqual(1, l.ndim)
+            self.assertEqual(2, s.ndim)
+            self.assertEqual((n_samples,), l.shape)
+            self.assertEqual((n_samples, n_classes), s.shape)
+            self.assertEqual(l.dtype, int)
+            self.assertEqual(s.dtype, float)
+
+        self.dec_tree.train(self.dataset)
+
+        # Testing discriminant_function on multiple points
+
+        df_scores_0 = self.dec_tree.discriminant_function(
+            self.dataset.X, label=0)
+        self.logger.info("discriminant_function("
+                         "dataset.X, label=0:\n{:}".format(df_scores_0))
+        _check_df_scores(df_scores_0, self.dataset.num_samples)
+
+        df_scores_1 = self.dec_tree.discriminant_function(
+            self.dataset.X, label=1)
+        self.logger.info("discriminant_function("
+                         "dataset.X, label=1:\n{:}".format(df_scores_1))
+        _check_df_scores(df_scores_1, self.dataset.num_samples)
+
+        df_scores_2 = self.dec_tree.discriminant_function(
+            self.dataset.X, label=2)
+        self.logger.info("discriminant_function("
+                         "dataset.X, label=2:\n{:}".format(df_scores_2))
+        _check_df_scores(df_scores_2, self.dataset.num_samples)
+
+        # Testing _discriminant_function on multiple points
+
+        ds_priv_scores_0 = self.dec_tree._discriminant_function(
+            self.dataset.X, label=0)
+        self.logger.info("_discriminant_function("
+                         "dataset.X, label=0:\n{:}".format(ds_priv_scores_0))
+        _check_df_scores(ds_priv_scores_0, self.dataset.num_samples)
+
+        ds_priv_scores_1 = self.dec_tree._discriminant_function(
+            self.dataset.X, label=1)
+        self.logger.info("_discriminant_function("
+                         "dataset.X, label=1:\n{:}".format(ds_priv_scores_1))
+        _check_df_scores(ds_priv_scores_1, self.dataset.num_samples)
+
+        ds_priv_scores_2 = self.dec_tree._discriminant_function(
+            self.dataset.X, label=2)
+        self.logger.info("_discriminant_function("
+                         "dataset.X, label=2:\n{:}".format(ds_priv_scores_2))
+        _check_df_scores(ds_priv_scores_2, self.dataset.num_samples)
+
+        # Comparing output of public and private
+
+        self.assertFalse((df_scores_0 != ds_priv_scores_0).any())
+        self.assertFalse((df_scores_1 != ds_priv_scores_1).any())
+        self.assertFalse((df_scores_2 != ds_priv_scores_2).any())
+
+        # Testing classify on multiple points
+
+        labels, scores = self.dec_tree.classify(self.dataset.X)
+        self.logger.info("classify(dataset.X:\nlabels: {:}"
+                         "\nscores:{:}".format(labels, scores))
+        _check_classify_scores(
+            labels, scores, self.dataset.num_samples, self.dec_tree.n_classes)
+
+        # Comparing output of discriminant_function and classify
+
+        self.assertFalse((df_scores_0 != scores[:, 0].ravel()).any())
+        self.assertFalse((df_scores_1 != scores[:, 1].ravel()).any())
+        self.assertFalse((df_scores_2 != scores[:, 2].ravel()).any())
+
+        # Testing discriminant_function on single point
+
+        df_scores_0 = self.dec_tree.discriminant_function(
+            self.dataset.X[0, :].ravel(), label=0)
+        self.logger.info("discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=0:\n{:}".format(df_scores_0))
+        _check_df_scores(df_scores_0, 1)
+
+        df_scores_1 = self.dec_tree.discriminant_function(
+            self.dataset.X[0, :].ravel(), label=1)
+        self.logger.info("discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=1:\n{:}".format(df_scores_1))
+        _check_df_scores(df_scores_1, 1)
+
+        df_scores_2 = self.dec_tree.discriminant_function(
+            self.dataset.X[0, :].ravel(), label=2)
+        self.logger.info("discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=2:\n{:}".format(df_scores_2))
+        _check_df_scores(df_scores_2, 1)
+
+        # Testing _discriminant_function on single point
+
+        df_priv_scores_0 = self.dec_tree._discriminant_function(
+            self.dataset.X[0, :].ravel(), label=0)
+        self.logger.info("_discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=0:\n{:}".format(df_priv_scores_0))
+        _check_df_scores(df_priv_scores_0, 1)
+
+        df_priv_scores_1 = self.dec_tree._discriminant_function(
+            self.dataset.X[0, :].ravel(), label=1)
+        self.logger.info("_discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=1:\n{:}".format(df_priv_scores_1))
+        _check_df_scores(df_priv_scores_1, 1)
+
+        df_priv_scores_2 = self.dec_tree._discriminant_function(
+            self.dataset.X[0, :].ravel(), label=2)
+        self.logger.info("_discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=2:\n{:}".format(df_priv_scores_2))
+        _check_df_scores(df_priv_scores_2, 1)
+
+        # Comparing output of public and private
+
+        self.assertFalse((df_scores_0 != df_priv_scores_0).any())
+        self.assertFalse((df_scores_1 != df_priv_scores_1).any())
+        self.assertFalse((df_scores_2 != df_priv_scores_2).any())
+
+        self.logger.info("Testing classify on single point")
+
+        labels, scores = self.dec_tree.classify(self.dataset.X[0, :].ravel())
+        self.logger.info("classify(self.dataset.X[0, :].ravel():\nlabels: "
+                         "{:}\nscores:{:}".format(labels, scores))
+        _check_classify_scores(labels, scores, 1, self.dec_tree.n_classes)
+
+        # Comparing output of discriminant_function and classify
+
+        self.assertFalse(
+            (df_scores_0 != CArray(scores[:, 0]).ravel()).any())
+        self.assertFalse(
+            (df_scores_1 != CArray(scores[:, 1]).ravel()).any())
+        self.assertFalse(
+            (df_scores_2 != CArray(scores[:, 2]).ravel()).any())
+        
 
 if __name__ == '__main__':
-    unittest.main()
+    CUnitTest.main()
--- src/secml/classifiers/tests/test_knn_classifier.py
+++ src/secml/classifiers/tests/test_knn_classifier.py
@@ -1,12 +1,12 @@
-import unittest
-import numpy as np
+from secml.utils import CUnitTest
 
+import numpy as np
 import pylab as pl
 from matplotlib.colors import ListedColormap
 
-from secml.utils import CUnitTest
 from secml.classifiers import CClassifierKNN
 from secml.data import CDataset
+from secml.array import CArray
 from secml.data.loader import CDLRandom
 from secml.peval.metrics import CMetricAccuracy
 
@@ -16,17 +16,30 @@ class TestCClassifierKNN(CUnitTest):
 
     def setUp(self):
 
-        ds = CDLRandom(n_samples=100, n_features=2, n_redundant=0,
-                       n_informative=1, n_clusters_per_class=1,
+        ds = CDLRandom(n_samples=100, n_classes=3, n_features=2,
+                       n_redundant=0, n_informative=2, n_clusters_per_class=1,
                        random_state=10000).load()
-        self.logger.info("Initializing KNeighbors Classifier... ")
+
         self.dataset = ds[:50, :]
         self.test = ds[50:, :]
-        self.KN_classifier = CClassifierKNN(n_neighbors=3)
-        self.KN_classifier.train(self.dataset)
+
+        self.logger.info("Initializing KNeighbors Classifier... ")
+        self.knn = CClassifierKNN(n_neighbors=3)
+        self.knn.train(self.dataset)
 
     def test_plot_dataset(self):
         self.logger.info("Draw the dataset... ")
+
+        ds = CDLRandom(n_samples=100, n_classes=2, n_features=2,
+                       n_redundant=0, n_informative=1, n_clusters_per_class=1,
+                       random_state=10000).load()
+
+        self.logger.info("Initializing KNeighbors Classifier... ")
+        self.dataset = ds[:50, :]
+        self.test = ds[50:, :]
+
+        self.knn = CClassifierKNN(n_neighbors=3)
+        self.knn.train(self.dataset)
 
         x_min = self.dataset.X[:, 0].min() - 1
         x_max = self.dataset.X[:, 0].max() + 1
@@ -36,7 +49,7 @@ class TestCClassifierKNN(CUnitTest):
         xx, yy = np.meshgrid(np.arange(x_min, x_max, self.step),
                              np.arange(y_min, y_max, self.step))
         self.grid = CDataset(np.c_[xx.ravel(), yy.ravel()])
-        lab, Z_tree = self.KN_classifier.classify(self.grid.X)
+        lab, Z_tree = self.knn.classify(self.grid.X)
         Z_tree = Z_tree[:, 1]
         Z_tree = Z_tree.reshape(xx.shape)
         cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
@@ -53,7 +66,7 @@ class TestCClassifierKNN(CUnitTest):
     def test_classification(self):
         self.logger.info("Check the classification method... ")
 
-        lab_cl, score = self.KN_classifier.classify(self.test.X)
+        lab_cl, score = self.knn.classify(self.test.X)
 
         acc = CMetricAccuracy().performance_score(self.test.Y, lab_cl)
 
@@ -71,7 +84,7 @@ class TestCClassifierKNN(CUnitTest):
         self.logger.info("Checking KNN classifier on a single sample...")
         num_samp = 3
         with self.timer():
-            dist, index_n, corresp = self.KN_classifier.kneighbors(
+            dist, index_n, corresp = self.knn.kneighbors(
                 single_sample, num_samp)
         self.logger.info("Sample to evaluate: {:}".format(single_sample))
         self.logger.info("")
@@ -83,13 +96,161 @@ class TestCClassifierKNN(CUnitTest):
         self.logger.info("Checking KNN classifier on multiple samples...")
         num_samp = 2
         with self.timer():
-            dist, index_n, corresp = self.KN_classifier.kneighbors(
+            dist, index_n, corresp = self.knn.kneighbors(
                 array_samples, num_samp)
-        for i in xrange(0, 10):
+        for i in xrange(10):
             self.logger.info("Sample to evaluate: {:}".format(single_sample))
             self.logger.info("Closest: {:}, index {:}, distance {:}"
                              "".format(corresp[i, :], index_n[i], dist[i, :]))
 
+    def test_fun(self):
+        """Test for discriminant_function() and classify() methods."""
+        self.logger.info(
+            "Test for discriminant_function() and classify() methods.")
+
+        def _check_df_scores(s, n_samples):
+            self.assertEqual(type(s), CArray)
+            self.assertTrue(s.isdense)
+            self.assertEqual(1, s.ndim)
+            self.assertEqual((n_samples,), df_scores_0.shape)
+            self.assertEqual(df_scores_0.dtype, float)
+
+        def _check_classify_scores(l, s, n_samples, n_classes):
+            self.assertEqual(type(l), CArray)
+            self.assertEqual(type(s), CArray)
+            self.assertTrue(l.isdense)
+            self.assertTrue(s.isdense)
+            self.assertEqual(1, l.ndim)
+            self.assertEqual(2, s.ndim)
+            self.assertEqual((n_samples,), l.shape)
+            self.assertEqual((n_samples, n_classes), s.shape)
+            self.assertEqual(l.dtype, int)
+            self.assertEqual(s.dtype, float)
+
+        self.knn.train(self.dataset)
+
+        # Testing discriminant_function on multiple points
+
+        df_scores_0 = self.knn.discriminant_function(
+            self.dataset.X, label=0)
+        self.logger.info("discriminant_function("
+                         "dataset.X, label=0:\n{:}".format(df_scores_0))
+        _check_df_scores(df_scores_0, self.dataset.num_samples)
+
+        df_scores_1 = self.knn.discriminant_function(
+            self.dataset.X, label=1)
+        self.logger.info("discriminant_function("
+                         "dataset.X, label=1:\n{:}".format(df_scores_1))
+        _check_df_scores(df_scores_1, self.dataset.num_samples)
+
+        df_scores_2 = self.knn.discriminant_function(
+            self.dataset.X, label=2)
+        self.logger.info("discriminant_function("
+                         "dataset.X, label=2:\n{:}".format(df_scores_2))
+        _check_df_scores(df_scores_2, self.dataset.num_samples)
+
+        # Testing _discriminant_function on multiple points
+
+        ds_priv_scores_0 = self.knn._discriminant_function(
+            self.dataset.X, label=0)
+        self.logger.info("_discriminant_function("
+                         "dataset.X, label=0:\n{:}".format(ds_priv_scores_0))
+        _check_df_scores(ds_priv_scores_0, self.dataset.num_samples)
+
+        ds_priv_scores_1 = self.knn._discriminant_function(
+            self.dataset.X, label=1)
+        self.logger.info("_discriminant_function("
+                         "dataset.X, label=1:\n{:}".format(ds_priv_scores_1))
+        _check_df_scores(ds_priv_scores_1, self.dataset.num_samples)
+
+        ds_priv_scores_2 = self.knn._discriminant_function(
+            self.dataset.X, label=2)
+        self.logger.info("_discriminant_function("
+                         "dataset.X, label=2:\n{:}".format(ds_priv_scores_2))
+        _check_df_scores(ds_priv_scores_2, self.dataset.num_samples)
+
+        # Comparing output of public and private
+
+        self.assertFalse((df_scores_0 != ds_priv_scores_0).any())
+        self.assertFalse((df_scores_1 != ds_priv_scores_1).any())
+        self.assertFalse((df_scores_2 != ds_priv_scores_2).any())
+
+        # Testing classify on multiple points
+
+        labels, scores = self.knn.classify(self.dataset.X)
+        self.logger.info("classify(dataset.X:\nlabels: {:}"
+                         "\nscores:{:}".format(labels, scores))
+        _check_classify_scores(
+            labels, scores, self.dataset.num_samples, self.knn.n_classes)
+
+        # Comparing output of discriminant_function and classify
+
+        self.assertFalse((df_scores_0 != scores[:, 0].ravel()).any())
+        self.assertFalse((df_scores_1 != scores[:, 1].ravel()).any())
+        self.assertFalse((df_scores_2 != scores[:, 2].ravel()).any())
+
+        # Testing discriminant_function on single point
+
+        df_scores_0 = self.knn.discriminant_function(
+            self.dataset.X[0, :].ravel(), label=0)
+        self.logger.info("discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=0:\n{:}".format(df_scores_0))
+        _check_df_scores(df_scores_0, 1)
+
+        df_scores_1 = self.knn.discriminant_function(
+            self.dataset.X[0, :].ravel(), label=1)
+        self.logger.info("discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=1:\n{:}".format(df_scores_1))
+        _check_df_scores(df_scores_1, 1)
+
+        df_scores_2 = self.knn.discriminant_function(
+            self.dataset.X[0, :].ravel(), label=2)
+        self.logger.info("discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=2:\n{:}".format(df_scores_2))
+        _check_df_scores(df_scores_2, 1)
+
+        # Testing _discriminant_function on single point
+
+        df_priv_scores_0 = self.knn._discriminant_function(
+            self.dataset.X[0, :].ravel(), label=0)
+        self.logger.info("_discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=0:\n{:}".format(df_priv_scores_0))
+        _check_df_scores(df_priv_scores_0, 1)
+
+        df_priv_scores_1 = self.knn._discriminant_function(
+            self.dataset.X[0, :].ravel(), label=1)
+        self.logger.info("_discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=1:\n{:}".format(df_priv_scores_1))
+        _check_df_scores(df_priv_scores_1, 1)
+
+        df_priv_scores_2 = self.knn._discriminant_function(
+            self.dataset.X[0, :].ravel(), label=2)
+        self.logger.info("_discriminant_function(dataset.X[0, :].ravel(), "
+                         "label=2:\n{:}".format(df_priv_scores_2))
+        _check_df_scores(df_priv_scores_2, 1)
+
+        # Comparing output of public and private
+
+        self.assertFalse((df_scores_0 != df_priv_scores_0).any())
+        self.assertFalse((df_scores_1 != df_priv_scores_1).any())
+        self.assertFalse((df_scores_2 != df_priv_scores_2).any())
+
+        self.logger.info("Testing classify on single point")
+
+        labels, scores = self.knn.classify(self.dataset.X[0, :].ravel())
+        self.logger.info("classify(self.dataset.X[0, :].ravel():\nlabels: "
+                         "{:}\nscores:{:}".format(labels, scores))
+        _check_classify_scores(labels, scores, 1, self.knn.n_classes)
+
+        # Comparing output of discriminant_function and classify
+
+        self.assertFalse(
+            (df_scores_0 != CArray(scores[:, 0]).ravel()).any())
+        self.assertFalse(
+            (df_scores_1 != CArray(scores[:, 1]).ravel()).any())
+        self.assertFalse(
+            (df_scores_2 != CArray(scores[:, 2]).ravel()).any())
+
 
 if __name__ == '__main__':
-    unittest.main()
+    CUnitTest.main()
