e38f11cda9bd5e07928e2653537ad113bf1f9829
Marco Melis <marco.melis@diee.unica.it>
2018-10-09 20:11:31 +0200

    Reviewed CLossSquared (now CLossQuadratic #123). Added CLossSquare.

--- src/secml/classifiers/loss/c_loss_squared.py
+++ src/secml/classifiers/loss/c_loss_squared.py
@@ -1,47 +1,159 @@
 """
-.. module:: SquaredLoss
-   :synopsis: Squared Loss Function
+.. module:: CLossSquare & CLossQuadratic
+   :synopsis: Squared Loss Functions
 
 .. moduleauthor:: Marco Melis <marco.melis@diee.unica.it>
 .. moduleauthor:: Ambra Demontis <ambra.demontis@diee.unica.it>
 
 """
-from secml.classifiers.loss import CLoss
+from secml.classifiers.loss import CLossRegression, CLossClassification
+from secml.classifiers.loss.c_loss import _check_binary_score
+from secml.classifiers.clf_utils import convert_binary_labels
 
 
-class CLossSquared(CLoss):
-    """Squared Loss Function.
+class CLossSquare(CLossClassification):
+    """Square Loss Function.
 
-    Ordinary least squares fit.
+    The square loss is defined as:
+
+    .. math::
+
+        L_\text{Square}(y, s) = \left\(1 - sy\right\)
+
+    Attributes
+    ----------
+    class_type : 'square'
+    suitable_for : 'classification'
 
     """
+    class_type = 'square'
 
-    class_type = 'squared_loss'
-    loss_type = 'regression'
-
-    # TODO: extend binary labels
-    def loss(self, y, score):
-        """Compute Squared Loss.
+    def loss(self, y_true, score, pos_label=1):
+        """Computes the value of the squared epsilon-insensitive loss function.
 
         Parameters
         ----------
-        y : CArray
-            Vector-like array.
+        y_true : CArray
+            Ground truth (correct), targets. Vector-like array.
         score : CArray
-            Vector-like array.
+            Outputs (predicted), targets.
+            2-D array of shape (n_samples, n_classes) or vector-like
+             array of shape (n_samples,). If vector-like, the probabilities
+             provided are assumed to be that of the positive class.
+        pos_label : {0, 1}, optional
+            The class wrt compute the loss function. Default 1.
+            If `score` is vector-like, this parameter is ignored.
+
+        Returns
+        -------
+        CArray
+            Loss function. Vector-like array.
 
         """
-        return 0.5 * ((score - y) ** 2)
+        if pos_label not in (0, 1):
+            raise ValueError("only {0, 1} are accepted for `pos_label`")
 
-    def dloss(self, y, score):
-        """Compute Squared Loss Derivative.
+        y_true = convert_binary_labels(y_true).ravel()  # Convert to {-1, 1}
+        score = _check_binary_score(score, pos_label)
+
+        return (1.0 - y_true * score) ** 2
+
+    def dloss(self, y_true, score, pos_label=1):
+        """Computes the derivative of the square loss function with respect to `score`.
 
         Parameters
         ----------
-        y : CArray
-            Vector-like array.
+        y_true : CArray
+            Ground truth (correct), targets. Vector-like array.
         score : CArray
-            Vector-like array.
+            Outputs (predicted), targets.
+            2-D array of shape (n_samples, n_classes) or vector-like
+             array of shape (n_samples,). If vector-like, the probabilities
+             provided are assumed to be that of the positive class.
+        pos_label : {0, 1}, optional
+            The class wrt compute the loss function derivative. Default 1.
+            If `score` is vector-like, this parameter is ignored.
+
+        Returns
+        -------
+        CArray
+            Derivative of the loss function. Vector-like array.
 
         """
-        return score - y
+        if pos_label not in (0, 1):
+            raise ValueError("only {0, 1} are accepted for `pos_label`")
+
+        y_true = convert_binary_labels(y_true).ravel()  # Convert to {-1, 1}
+        score = _check_binary_score(score, pos_label)
+
+        return -2.0 * y_true * (1.0 - y_true * score)
+
+
+class CLossQuadratic(CLossRegression):
+    """Quadratic Loss Function (Ordinary Least Squares).
+
+    The quadratic loss is defined as:
+
+    .. math::
+
+        L_\text{Quadratic}(y, s) = \frac{1}{2}\left\(s - y\right\)^2
+
+    Attributes
+    ----------
+    class_type : 'quadratic'
+    suitable_for : 'regression'
+
+    """
+    class_type = 'quadratic'
+
+    def loss(self, y_true, score):
+        """Computes the value of the quadratic loss function.
+
+        Parameters
+        ----------
+        y_true : CArray
+            Ground truth (correct), targets. Vector-like array.
+        score : CArray
+            Outputs (predicted), targets.
+            Vector-like array of shape (n_samples,).
+
+        Returns
+        -------
+        CArray
+            Loss function. Vector-like array.
+
+        """
+        if score.is_vector_like is not None:
+            raise ValueError("only vector-like score array is supported.")
+
+        # Ensure we work with vector-like arrays
+        y_true = y_true.ravel()
+        score = score.ravel()
+
+        return 0.5 * ((score - y_true) ** 2)
+
+    def dloss(self, y_true, score):
+        """Computes the derivative of the quadratic loss function with respect to `score`.
+
+        Parameters
+        ----------
+        y_true : CArray
+            Ground truth (correct), targets. Vector-like array.
+        score : CArray
+            Outputs (predicted), targets.
+            Vector-like array of shape (n_samples,).
+
+        Returns
+        -------
+        CArray
+            Derivative of the loss function. Vector-like array.
+
+        """
+        if score.is_vector_like is not None:
+            raise ValueError("only vector-like score array is supported.")
+
+        # Ensure we work with vector-like arrays
+        y_true = y_true.ravel()
+        score = score.ravel()
+
+        return score - y_true
