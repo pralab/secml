0fa4ef984f09eac7e4c5cf4a674157c6c00d094b
Marco Melis <marco.melis@diee.unica.it>
2018-10-10 19:18:32 +0200

    Reviewed CLossLogistic.

--- src/secml/classifiers/loss/c_loss_logistic.py
+++ src/secml/classifiers/loss/c_loss_logistic.py
@@ -1,91 +1,111 @@
 """
-.. module:: Logistic loss
-   :synopsis: Logistic loss (log loss, cross-entropy loss)
+.. module:: CLossLogistic
+   :synopsis: Logistic loss function
 
 .. moduleauthor:: Battista Biggio <battista.biggio@diee.unica.it>
+.. moduleauthor:: Marco Melis <marco.melis@diee.unica.it>
 
 """
-from secml.classifiers.loss import CLoss
+from secml.classifiers.loss import CLossClassification
+from secml.classifiers.loss.c_loss import _check_binary_score
 from secml.classifiers.clf_utils import convert_binary_labels
 from secml.array import CArray
 
 
-class CLossLogistic(CLoss):
-    """Hinge Loss Function (soft-margin).
-
-    Useful to construct Support Vector Machines.
-
-    """
-
+class CLossLogistic(CLossClassification):
+    """Logistic loss function."""
     class_type = 'logistic'
-    loss_type = 'classification'
 
-    def __init__(self, extend_binary_labels=True):
-        # this maps standard binary labels {0, 1} to {-1, 1}
-        self._extend_binary_labels = bool(extend_binary_labels)
-
-        # set upper bound for a linear approximation when -yf is large
-        # to avoid numerical overflows
-        # the linear approximation is fine beyond 10:
-        #   log(1+exp(10)) = 10.000045
-        self._bound = 10
-
-
-    def loss(self, y, score):
-        """Compute Logistic Loss.
-        log ( 1+ exp(-yf))
+    def loss(self, y_true, score, pos_label=1, bound=10):
+        """Computes the value of the logistic loss function.
 
         Parameters
         ----------
-        y : CArray
-            Vector-like array.
+        y_true : CArray
+            Ground truth (correct), targets. Vector-like array.
         score : CArray
-            Vector-like array.
+            Outputs (predicted), targets.
+            2-D array of shape (n_samples, n_classes) or vector-like
+             array of shape (n_samples,). If vector-like, the probabilities
+             provided are assumed to be that of the positive class.
+        pos_label : {0, 1}, optional
+            The class wrt compute the loss function. Default 1.
+            If `score` is vector-like, this parameter is ignored.
+        bound : scalar or None, optional
+            Set an upper bound for a linear approximation when -y*s is large
+             to avoid numerical overflows.
+            10 is a generally acceptable -> log(1+exp(10)) = 10.000045
+
+        Returns
+        -------
+        CArray
+            Loss function. Vector-like array.
 
         """
-        if self._extend_binary_labels:
-            y = convert_binary_labels(y)
+        if pos_label not in (0, 1):
+            raise ValueError("only {0, 1} are accepted for `pos_label`")
 
-        v = CArray(- y * score).astype(float)
+        y_true = convert_binary_labels(y_true).ravel()  # Convert to {-1, 1}
+        score = _check_binary_score(score, pos_label)
 
-        if self._bound is None:
-            h = (1.0 + v.exp()).log()
+        # log(1 + exp(-y*s))
+        v = CArray(- y_true * score)
+
+        if bound is None:
+            v = (1.0 + v.exp())
+
         else:
             # linear approximation avoids numerical overflows
-            h = v.astype(float)  # when -yf >> 1 : log ( 1+ exp(-yf)) ~= -yf
-            h[v < self._bound] = (1.0 + CArray(v[v < self._bound]).exp()).log()
-        return h
+            # when -yf >> 1 : log ( 1+ exp(-yf)) ~= -yf
+            v[v < bound] = (1.0 + CArray(v[v < bound]).exp()).log()
 
-    def dloss(self, y, score):
-        """Compute Loss Derivative w.r.t. f
-        d log ( 1+ exp(-yf)) / df =
-            1/( 1+ exp(-yf)) exp(-yf) -y
+        return v
+
+    def dloss(self, y_true, score, pos_label=1, bound=10):
+        """Computes the derivative of the hinge loss function with respect to `score`.
 
         Parameters
         ----------
-        y : CArray
-            Vector-like array.
+        y_true : CArray
+            Ground truth (correct), targets. Vector-like array.
         score : CArray
-            Vector-like array.
+            Outputs (predicted), targets.
+            2-D array of shape (n_samples, n_classes) or vector-like
+             array of shape (n_samples,). If vector-like, the probabilities
+             provided are assumed to be that of the positive class.
+        pos_label : {0, 1}, optional
+            The class wrt compute the loss function derivative. Default 1.
+            If `score` is vector-like, this parameter is ignored.
+        bound : scalar or None, optional
+            Set an upper bound for a linear approximation when -y*s is large
+             to avoid numerical overflows.
+            10 is a generally acceptable -> log(1+exp(10)) = 10.000045
+
+        Returns
+        -------
+        CArray
+            Derivative of the loss function. Vector-like array.
 
         """
+        if pos_label not in (0, 1):
+            raise ValueError("only {0, 1} are accepted for `pos_label`")
 
-        if self._extend_binary_labels:
-            y = convert_binary_labels(y)
+        y_true = convert_binary_labels(y_true).ravel()  # Convert to {-1, 1}
+        score = _check_binary_score(score, pos_label)
 
-        v = CArray(- y * score).astype(float)
+        # d log ( 1+ exp(-yf)) / df =
+        #     1/( 1+ exp(-yf)) exp(-yf) -y
 
-        if self._bound is None:
-            h = -y * v.exp() / (1.0 + v.exp())
+        v = CArray(- y_true * score)
+
+        if bound is None:
+            h = -y_true * v.exp() / (1.0 + v.exp())
+
         else:
-            if v.size > 1:
-                # linear approximation avoids numerical overflows
-                # when -yf >> 1 : loss ~= -yf, and grad = -y
-                h = -y.astype(float)
-                h[v < self._bound] = -y[v < self._bound] * \
-                                     CArray(v[v < self._bound]).exp() / \
-                                     (1.0 + CArray(v[v < self._bound]).exp())
-            else:
-                # single point
-                h = -y * v.exp() / (1.0 + v.exp()) if v < self._bound else -y
+            # linear approximation avoids numerical overflows
+            # when -yf >> 1 : loss ~= -yf, and grad = -y
+            h = -y_true
+            h[v < bound] = h[v < bound] * CArray(v[v < bound]).exp() / \
+                (1.0 + CArray(v[v < bound]).exp())
+
         return h
