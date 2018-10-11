b201822deb4e2595e685f058a3ea4183967c11a4
Marco Melis <marco.melis@diee.unica.it>
2018-10-11 20:27:54 +0200

    Added missing CLossSquare to __init__

--- src/secml/classifiers/loss/__init__.py
+++ src/secml/classifiers/loss/__init__.py
@@ -2,6 +2,6 @@ from c_loss import CLoss, CLossRegression, CLossClassification
 from c_loss_epsilon_insensitive import \
     CLossEpsilonInsensitive, CLossEpsilonInsensitiveSquared
 from c_loss_hinge import CLossHinge, CLossHingeSquared
-from c_loss_squared import CLossQuadratic
+from c_loss_squared import CLossSquare, CLossQuadratic
 from c_loss_cross_entropy import softmax, CLossCrossEntropy
 from c_loss_logistic import CLossLogistic
