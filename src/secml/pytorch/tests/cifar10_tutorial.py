a9afbbf9b4652c837bb7ee920b5bdec28ec2093c
Marco Melis <marco.melis@diee.unica.it>
2018-10-12 16:39:25 +0200

    Fixed #134: the correct value of workers to mimic n_jobs is now passed to num_workers param of torch.DataLoader

--- src/secml/pytorch/tests/cifar10_tutorial.py
+++ src/secml/pytorch/tests/cifar10_tutorial.py
@@ -82,7 +82,7 @@ trainset = CTorchDataset(tr, transform=transform)
 #     root=fm.join(SECML_PYTORCH_DATA_DIR, 'data'),
 #     train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+                                          shuffle=True, num_workers=1)
 
 testset = CTorchDataset(ts, transform=transform)
 
@@ -90,7 +90,7 @@ testset = CTorchDataset(ts, transform=transform)
 #     root=fm.join(SECML_PYTORCH_DATA_DIR, 'data'),
 #     train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+                                         shuffle=False, num_workers=1)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
