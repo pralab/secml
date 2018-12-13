07e2f5421e575167d2d8b49657208f6328f91216
Marco Melis <marco.melis@diee.unica.it>
2018-12-13 14:47:13 +0100

    __init__ and setup now read version.py. Still few duplications to sort out.

--- src/secml/__init__.py
+++ src/secml/__init__.py
@@ -1,15 +1,4 @@
-import os
-import io
-from pkg_resources import parse_version
-
 from ._globals import _NoValue
 from .core.settings import SECML_CONFIG
 
-
-# From `VERSION` file (see https://packaging.pypa.io/en/latest/version/)
-version_p = os.path.join(os.path.dirname(__file__), 'VERSION')
-try:
-    with io.open(version_p, 'r') as _version_f:
-        __version__ = parse_version(_version_f.read().strip())
-except:
-    raise RuntimeError("Unable to find version string.")
+from version import VERSION as __version__
