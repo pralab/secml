6b2cac7ae6474b4a4c3b06b361cd5c97ae996b00
Marco Melis <marco.melis@diee.unica.it>
2018-12-14 19:24:58 +0100

    Resolve "Replace distutils with setuptools in setup"

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from setuptools import setup, find_packages
 from pkg_resources import parse_version
 import os
 import io
-import re
 import subprocess
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-def read(*parts):
+def read(*path_parts):
     # intentionally *not* adding an encoding option to open, See:
     #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with io.open(os.path.join(here, *parts), 'r') as fp:
+    with io.open(os.path.join(here, *path_parts), 'r') as fp:
         return fp.read().strip()
 
 
@@ -30,47 +29,130 @@ def git_version():
         env['LANG'] = 'C'
         env['LC_ALL'] = 'C'
         # Execute in the current dir
-        cwd = os.path.dirname(__file__)
-        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=cwd, env=env).communicate()[0]
+        out = subprocess.Popen(cmd, cwd=here, env=env,
+                               stdout=subprocess.PIPE,
+                               stderr=open(os.devnull, 'w')).communicate()[0]
         return out
 
     try:
         out = _minimal_ext_cmd(['git', 'rev-parse', '--short', 'HEAD'])
         GIT_REVISION = out.strip().decode('ascii')
+        if len(GIT_REVISION) == 0:
+            raise OSError
     except OSError:
         GIT_REVISION = 'Unknown'
 
     return GIT_REVISION
 
 
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^_VERSION = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        _v_raw = version_match.group(1)
+def find_version(*path_parts):
+    """This function read version number and revision number if available."""
+    try:
+        _v_f = read(*path_parts)  # Read main version file
         _v_git = git_version()
-        _v_raw = _v_raw if _v_git == 'Unknown' else _v_raw + '+' + _v_git
-        return str(parse_version(_v_raw))
+        if _v_git == 'Unknown':
+            try:  # Try to read rev from file. May not exists
+                _v_git = read('src', 'secml', 'VERSION_REV')
+            except:
+                pass  # _v_git will stay "Unknown"
+        else:
+            write_rev(_v_git, 'src', 'secml', 'VERSION_REV')
+        # Append rev number only if available
+        _v = _v_f if _v_git == 'Unknown' else _v_f + '+' + _v_git
+        _v = parse_version(_v)  # Integrity checks
+        # Display rev number (if available) for prereleases only
+        return str(_v) if _v.is_prerelease else _v.public
+    except:
+        raise RuntimeError("Unable to find version string.")
 
-    raise RuntimeError("Unable to find version string.")
 
+def write_rev(v, *path_parts):
+    """Write revision id to file."""
+    a = open(os.path.join(here, *path_parts), 'w')
+    try:
+        a.write(v)
+    finally:
+        a.close()
+
+
+def install_deps():
+    """Reads requirements.txt and preprocess it
+    to be feed into setuptools.
+
+    This is the only possible way (we found)
+    how requirements.txt can be reused in setup.py
+    using dependencies from private github repositories.
+
+    Links must be appended by `-{StringWithAtLeastOneNumber}`
+    or something like that, so e.g. `-9231` works as well as
+    `1.1.0`. This is ignored by the setuptools, but has to be there.
+
+    Warnings
+    --------
+    To make pip respect the links, you have to use
+    `--process-dependency-links` switch. So e.g.:
+    `pip install --process-dependency-links {git-url}`
+
+    Returns
+    -------
+    List of packages and dependency links.
+
+    Notes
+    -----
+    Thanks to knykda for this implementation:
+        https://github.com/pypa/pip/issues/3610#issuecomment-356687173
+
+    """
+    default = io.open(os.path.join(here, 'requirements.txt'), 'r').readlines()
+    new_pkgs = []
+    links = []
+    for resource in default:
+        if 'git+ssh' in resource:
+            pkg = resource.split('#')[-1]
+            links.append(resource.strip() + '-9876543210')
+            new_pkgs.append(pkg.replace('egg=', '').rstrip())
+        else:
+            new_pkgs.append(resource.strip())
+    return new_pkgs, links
+
+
+REQ_PKGS, DEP_LINKS = install_deps()
 
 LONG_DESCRIPTION = read('README.md')
 
+CLASSIFIERS = """\
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: Implementation :: PyPy
+Topic :: Software Development
+Topic :: Scientific/Engineering
+Operating System :: POSIX
+Operating System :: Unix
+"""
+
 setup(
     name='SecML-Lib',
-    version=find_version("src", "secml", "version.py"),
+    version=find_version("src", "secml", "VERSION"),
     description='A library for Secure Machine Learning',
     long_description=LONG_DESCRIPTION,
     license='GNU GPLv3',
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    platforms=["Linux", "Unix"],
     url='https://sec-ml.pluribus-one.it/lib/',
-    download_url='https://pypi.org/project/secml-lib/#files',
+    download_url=
+        'git+ssh://git@pragit.diee.unica.it/secml/secml-lib.git#egg=secml-lib',
     maintainer='Marco Melis',
     maintainer_email='marco.melis@diee.unica.it',
     packages=find_packages('src', exclude=["*.tests", "*.tests.*",
                                            "tests.*", "tests"]),
     package_dir={'': 'src'},
     include_package_data=True,
-    python_requires='==2.7.*'
+    python_requires='==2.7.*',
+    install_requires=REQ_PKGS,
+    zip_safe=True
 )
