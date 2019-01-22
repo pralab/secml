ba540fca5dde336665573debcd411c76d304d684
Ambra <ambra.demontis@diee.unica.it>
2019-01-22 13:42:09 +0100

    split the script in different functions

--- src/secml/ml/classifiers/tests/test_c_classifier_gradient.py
+++ src/secml/ml/classifiers/tests/test_c_classifier_gradient.py
@@ -10,19 +10,26 @@ from secml.utils import CUnitTest
 
 class TestCClassifierGradient(CUnitTest):
 
-    def setUp(self):
+    def _dataset_creation(self):
 
         # generate synthetic data
-        self.dataset = CDLRandom(n_features=2, n_redundant=0, n_informative=1,
+        self.dataset = CDLRandom(n_features=2, n_redundant=0, n_informative=2,
                                  n_clusters_per_class=1, random_state=1).load()
 
         self.dataset_sparse = self.dataset.tosparse()
+
+    def _clfs_creation(self):
 
         self.clfs = [CClassifierSVM()]
         self.clf_ids = ['lin-SVM']
 
         for clf in self.clfs:  # Enabling debug output for each classifier
             clf.verbose = 2
+
+    def setUp(self):
+
+        self._dataset_creation()
+        self._clfs_creation()
 
         self.logger.info("." * 50)
         self.logger.info("Number of Patterns: %s",
