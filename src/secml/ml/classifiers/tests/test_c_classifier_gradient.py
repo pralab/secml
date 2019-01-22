ee49721b2100193d6d98b4429b0964b9125e7c4b
Ambra <ambra.demontis@diee.unica.it>
2019-01-22 13:51:29 +0100

    split the gradient test file in two different scripts

--- src/secml/ml/classifiers/tests/test_c_classifier_gradient.py
+++ src/secml/ml/classifiers/tests/test_c_classifier_gradient.py
@@ -1,3 +1,6 @@
+from secml.utils import CUnitTest
+from abc import ABCMeta, abstractmethod
+
 import random
 
 from secml.data.loader import CDLRandom
@@ -5,91 +8,83 @@ from secml.ml.classifiers import CClassifierSVM
 from secml.ml.features.normalization import CNormalizerMinMax
 from secml.optimization import COptimizer
 from secml.optimization.function import CFunction
-from secml.utils import CUnitTest
 
 
-class TestCClassifierGradient(CUnitTest):
+class CClassifierGradientTestCases(object):
+    """Wrapper for TestCClassifierGradient to make unittest.main() work correctly."""
 
-    def _dataset_creation(self):
+    class TestCClassifierGradient(CUnitTest):
+        """Unit test for the classifier gradients."""
+        __metaclass__ = ABCMeta
 
-        # generate synthetic data
-        self.dataset = CDLRandom(n_features=2, n_redundant=0, n_informative=2,
-                                 n_clusters_per_class=1, random_state=1).load()
+        @abstractmethod
+        def _dataset_creation(self):
+            raise NotImplementedError
 
-        self.dataset_sparse = self.dataset.tosparse()
+        @abstractmethod
+        def _clfs_creation(self):
+            raise NotImplementedError
 
-    def _clfs_creation(self):
+        def setUp(self):
 
-        self.clfs = [CClassifierSVM()]
-        self.clf_ids = ['lin-SVM']
+            self._dataset_creation()
+            self._clfs_creation()
 
-        for clf in self.clfs:  # Enabling debug output for each classifier
-            clf.verbose = 2
+            self.logger.info("." * 50)
+            self.logger.info("Number of Patterns: %s",
+                             str(self.dataset.num_samples))
+            self.logger.info("Features: %s", str(self.dataset.num_features))
 
-    def setUp(self):
+        def _clf_gradient_check(self, clf, clf_idx):
 
-        self._dataset_creation()
-        self._clfs_creation()
+            i = random.sample(xrange(self.dataset.num_samples), 1)[0]
+            pattern = self.dataset.X[i, :]
+            self.logger.info("P {:}: {:}".format(i, pattern))
 
-        self.logger.info("." * 50)
-        self.logger.info("Number of Patterns: %s",
-                         str(self.dataset.num_samples))
-        self.logger.info("Features: %s", str(self.dataset.num_features))
+            for c in self.dataset.classes:
 
-    def _clf_gradient_check(self, clf, clf_idx):
+                # Compare the analytical grad with the numerical grad
+                gradient = clf.gradient_f_x(pattern, y=c)
+                self.logger.info("Gradient w.r.t. class %s: %s",str(c), str(
+                    gradient))
+                check_grad_val = COptimizer(
+                    CFunction(clf.decision_function,
+                              clf.gradient_f_x)).check_grad(pattern, y=c)
+                self.logger.info(
+                    "norm(grad - num_grad): %s", str(check_grad_val))
+                self.assertLess(check_grad_val, 1e-3,
+                                "problematic classifier is " +
+                                clf_idx)
+                for i, elm in enumerate(gradient):
+                    self.assertIsInstance(elm, float)
 
-        i = random.sample(xrange(self.dataset.num_samples), 1)[0]
-        pattern = self.dataset.X[i, :]
-        self.logger.info("P {:}: {:}".format(i, pattern))
+        def test_f_x_gradient(self):
+            """Test the gradient of the classifier discriminant function"""
+            self.logger.info("Testing the gradient of the discriminant function")
 
-        for c in self.dataset.classes:
+            for clf, clf_idx in zip(self.clfs, self.clf_ids):
+                self.logger.info("Computing gradient for the classifier: %s when "
+                                 "the classifier does not have a normalizer "
+                                 "inside", clf_idx)
 
-            # Compare the analytical grad with the numerical grad
-            gradient = clf.gradient_f_x(pattern, y=c)
-            self.logger.info("Gradient w.r.t. class %s: %s",str(c), str(
-                gradient))
-            check_grad_val = COptimizer(
-                CFunction(clf.decision_function,
-                          clf.gradient_f_x)).check_grad(pattern, y=c)
+                clf.fit(self.dataset)
+                self._clf_gradient_check(clf, clf_idx)
+
+        def test_f_norm_x_gradient(self):
+            """Test the gradient of the classifier discriminant function
+            when the classifier have a normalizer inside"""
             self.logger.info(
-                "norm(grad - num_grad): %s", str(check_grad_val))
-            self.assertLess(check_grad_val, 1e-3,
-                            "problematic classifier is " +
-                            clf_idx)
-            for i, elm in enumerate(gradient):
-                self.assertIsInstance(elm, float)
+                "Testing the gradient of the discriminant function when the "
+                "classifier have a normalizer inside")
 
-    def test_f_x_gradient(self):
-        """Test the gradient of the classifier discriminant function"""
-        self.logger.info("Testing the gradient of the discriminant function")
+            # create a normalizer
+            normalizer = CNormalizerMinMax()
 
-        for clf, clf_idx in zip(self.clfs, self.clf_ids):
-            self.logger.info("Computing gradient for the classifier: %s when "
-                             "the classifier does not have a normalizer "
-                             "inside", clf_idx)
+            for clf, clf_idx in zip(self.clfs, self.clf_ids):
+                self.logger.info("Computing gradient for the classifier: %s when "
+                                 "the classifier have a normalizer "
+                                 "inside", clf_idx)
 
-            clf.fit(self.dataset)
-            self._clf_gradient_check(clf, clf_idx)
-
-    def test_f_norm_x_gradient(self):
-        """Test the gradient of the classifier discriminant function
-        when the classifier have a normalizer inside"""
-        self.logger.info(
-            "Testing the gradient of the discriminant function when the "
-            "classifier have a normalizer inside")
-
-        # create a normalizer
-        normalizer = CNormalizerMinMax()
-
-        for clf, clf_idx in zip(self.clfs, self.clf_ids):
-            self.logger.info("Computing gradient for the classifier: %s when "
-                             "the classifier have a normalizer "
-                             "inside", clf_idx)
-
-            clf.preprocess = normalizer
-            clf.fit(self.dataset)
-            self._clf_gradient_check(clf, clf_idx)
-
-
-if __name__ == '__main__':
-    CUnitTest.main()
+                clf.preprocess = normalizer
+                clf.fit(self.dataset)
+                self._clf_gradient_check(clf, clf_idx)
