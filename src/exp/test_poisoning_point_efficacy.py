4b744fbfaa87be6937734d9a73e6e8054d1794ab
Ambra <ambra.demontis@diee.unica.it>
2019-01-28 20:27:31 +0100

    compute the accuracy on all the test samples and use tr2

--- src/exp/test_poisoning_point_efficacy.py
+++ src/exp/test_poisoning_point_efficacy.py
@@ -5,10 +5,10 @@ import torchvision.transforms as transforms
 
 from secml.pytorch.classifiers import CClassifierPyTorchCarliniCNNMNIST
 from secml.core import CCreator
-from secml.data.loader import CDataLoaderMNIST
 from secml.array import CArray
 from secml.data import CDataset
 from secml.ml.peval.metrics import CMetricAccuracy
+from dataset_creation import create_mnist_dataset
 
 
 class TestPoisoningPointEfficacy(CCreator):
@@ -17,37 +17,48 @@ class TestPoisoningPointEfficacy(CCreator):
     effectiveness into decrease the classifier accuracy.
     """
 
+    # def _load_mnist(self):
+    #     loader = CDataLoaderMNIST()
+    #
+    #     self._digits = [8, 9]
+    #     self.tr = loader.load('training', digits=self._digits)
+    #
+    #     print "classes: ", self.tr.classes
+    #     print "features ", self.tr.num_features
+    #
+    #     self.ts = loader.load('testing', digits=self._digits)
+    #
+    #     # get dataset img dimension
+    #     # TODO: these should not be included in CDataset!
+    #     # they're lost after conversion tosparse
+    #     self.img_w = self.tr.img_w
+    #     self.img_h = self.tr.img_h
+    #
+    #     self.tr.X /= 255.0
+    #     self.ts.X /= 255.0
+    #
+    #     idx = CArray.arange(0, self.tr.num_samples)
+    #     val_dts_idx = CArray.randsample(idx, 500, random_state=self.seed)
+    #     self._val = self.tr[val_dts_idx, :]
+    #
+    #     tr_dts_idx = CArray.randsample(idx, 1000, random_state=self.seed)
+    #     self.tr = self.tr[tr_dts_idx, :]
+    #
+    #     idx = CArray.arange(0, self.ts.num_samples)
+    #     ts_dts_idx = CArray.randsample(idx, 1000, random_state=self.seed)
+    #     self.ts = self.ts[ts_dts_idx, :]
+    #
+    #     self.transform_train = transforms.Compose([
+    #         transforms.Lambda(lambda x: x.reshape([1, 28, 28])),
+    #     ])
+
     def _load_mnist(self):
-        loader = CDataLoaderMNIST()
 
-        self._digits = [8, 9]
-        self.tr = loader.load('training', digits=self._digits)
+        self.tr, self.val, self.ts, self.tr2 = create_mnist_dataset(
+            seed=self.seed, digits=[8, 9], n_tr=500, n_val=1000, n_ts=1000)
 
-        print "classes: ", self.tr.classes
-        print "features ", self.tr.num_features
-
-        self.ts = loader.load('testing', digits=self._digits)
-
-        # get dataset img dimension
-        # TODO: these should not be included in CDataset!
-        # they're lost after conversion tosparse
-        self.img_w = self.tr.img_w
-        self.img_h = self.tr.img_h
-
-        self.tr.X /= 255.0
-        self.ts.X /= 255.0
-
-        idx = CArray.arange(0, self.tr.num_samples)
-        val_dts_idx = CArray.randsample(idx, 500, random_state=self.seed)
-        self._val = self.tr[val_dts_idx, :]
-
-        tr_dts_idx = CArray.randsample(idx, 1000, random_state=self.seed)
-        self.tr = self.tr[tr_dts_idx, :]
-
-        idx = CArray.arange(0, self.ts.num_samples)
-        ts_dts_idx = CArray.randsample(idx, 1000, random_state=self.seed)
-        self.ts = self.ts[ts_dts_idx, :]
-
+        # define the trasformation that will be applied just before to feed
+        # the samples to the neural network
         self.transform_train = transforms.Compose([
             transforms.Lambda(lambda x: x.reshape([1, 28, 28])),
         ])
@@ -62,12 +73,12 @@ class TestPoisoningPointEfficacy(CCreator):
             classifier accuracy
         """
         labels, scores = clf.predict(
-            self.ts[50:100, :].X, return_decision_function=True)
+            self.ts.X, return_decision_function=True)
 
         print("Labels:\n{:}".format(labels))
         print("Scores:\n{:}".format(scores))
 
-        acc = CMetricAccuracy().performance_score(self.ts[50:100, :].Y, labels)
+        acc = CMetricAccuracy().performance_score(self.ts.Y, labels)
 
         return acc
 
@@ -91,8 +102,9 @@ class TestPoisoningPointEfficacy(CCreator):
         self.seed = 0
 
         self.n_pois_points = 25
-        # self.pois_data_path = "/home/ambra/np_adv/mnist_0_logistic"
-        self.pois_data_path = "/home/ambra/np_adv_tr/mnist_0_logistic"
+        self.pois_data_path = "/home/ambra/np_adv/mnist_0_logistic"
+        # self.pois_data_path = "/home/ambra/np_adv_tr/mnist_0_logistic"
+        #self.pois_data_path = "/home/ambra/new_np_adv/secml_code"
 
         self._load_mnist()
 
@@ -104,7 +116,7 @@ class TestPoisoningPointEfficacy(CCreator):
 
         print("Test the efficacy of the poisoning point")
 
-        self.clf.fit(self.tr)
+        self.clf.fit(self.tr2)
 
         clear_acc = self._get_accuracy(self.clf)
         print("Accuracy of the classifier trained on the "
@@ -113,7 +125,7 @@ class TestPoisoningPointEfficacy(CCreator):
         self.clf.clear()
 
         pois_data = self._load_pois_data()
-        pois_dts = self.tr.append(pois_data)
+        pois_dts = self.tr2.append(pois_data)
         print "pois dts X shape ", pois_dts.X.shape
 
         self.clf.fit(pois_dts)
@@ -125,5 +137,3 @@ class TestPoisoningPointEfficacy(CCreator):
 
 test = TestPoisoningPointEfficacy()
 test.test_pois_efficacy()
-
-
