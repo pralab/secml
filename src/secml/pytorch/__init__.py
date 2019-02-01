0e69b1d28fbb42ba4d5e724b5f459d11d05ac1fa
Ambra <ambra.demontis@diee.unica.it>
2019-02-01 18:12:54 +0100

    remove folders that should not be merged

--- src/secml/pytorch/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    import torch
-except ImportError:
-    try:  # Skip unittests if torch is not available
-        from secml.utils import CUnitTest
-        CUnitTest.importskip("torch")
-    except ImportError:  # CUnitTest not available
-        pass
-    raise ImportError("PyTorch is not available!")
