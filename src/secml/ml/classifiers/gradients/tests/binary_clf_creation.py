9ff781ae4fb9a1e576a1855657dca14f79797947
Ambra <ambra.demontis@diee.unica.it>
2019-02-05 19:28:35 +0100

    fix dataset gen function

--- src/secml/ml/classifiers/gradients/tests/binary_clf_creation.py
+++ src/secml/ml/classifiers/gradients/tests/binary_clf_creation.py
@@ -4,12 +4,12 @@ from secml.ml.classifiers.regularizer import CRegularizerL2
 from secml.ml.classifiers import CClassifierSVM, CClassifierKDE, \
     CClassifierSGD, CClassifierMCSLinear, CClassifierLogistic, CClassifierRidge
 
-def clf_creation(clf_idx, normalizer=False):
+def binary_clf_creation(clf_idx, normalizer=False):
 
     if clf_idx == 'lin-svm':
         clf = CClassifierSVM()
     elif clf_idx == 'rbf-svm':
-        CClassifierSVM(kernel='rbf')
+        clf = CClassifierSVM(kernel='rbf')
     elif clf_idx == 'logistic':
         clf = CClassifierLogistic()
     elif clf_idx == 'ridge':
@@ -25,7 +25,7 @@ def clf_creation(clf_idx, normalizer=False):
     elif clf_idx == 'sgd-lin':
         clf = CClassifierSGD(CLossHinge(),CRegularizerL2())
     elif clf_idx == 'sgd-rbf':
-        CClassifierSGD(CLossHinge(), CRegularizerL2(),kernel='rbf')
+        clf = CClassifierSGD(CLossHinge(), CRegularizerL2(),kernel='rbf')
     else:
         raise ValueError("classifier idx not managed!")
 
