6082a53f61902dae8129f0f24e1d9c9ac18a4036
Ambra <ambra.demontis@diee.unica.it>
2019-02-05 11:16:04 +0100

    remove the functionalities to get the margin sv. Add derivatives

--- src/secml/ml/classifiers/gradients/c_classifier_gradient_svm.py
+++ src/secml/ml/classifiers/gradients/c_classifier_gradient_svm.py
@@ -9,29 +9,13 @@ class CClassifierGradientSVM(CClassifierGradient):
     def __init__(self):
         self._loss = CLossHinge()
 
-    def _s(self, clf, tol=1e-6):
-        """Indices of margin support vectors."""
-        s = clf.alpha.find(
-            (abs(clf.alpha) >= tol) *
-            (abs(clf.alpha) <= clf.C - tol))
-        return CArray(s)
-
-    def _xs(self, clf):
-        s = self._s(clf)
-
-        if s.size == 0:
-            return None
-
-        xs = clf.sv[s, :].atleast_2d()
-        return xs, s
-
     def hessian(self, clf):
         """
         Compute hessian of the loss w.r.t. the classifier parameters
         """
         svm = clf
 
-        xs, sv_idx = self._xs(clf)  # these points are already normalized
+        xs, sv_idx = clf.xs()  # these points are already normalized
 
         s = xs.shape[0]
 
@@ -41,32 +25,37 @@ class CClassifierGradientSVM(CClassifierGradient):
 
         return H
 
-    def fd_params(self, x, clf):
+    def fd_params(self, xk, clf):
         """
         Derivative of the discriminant function w.r.t. the classifier
         parameters
         """
-        xs, sv_idx = self._xs()  # these points are already normalized
+        xs, sv_idx = clf.xs()  # these points are already normalized
 
         if xs is None:
             self.logger.debug("Warning: xs is empty "
                               "(all points are error vectors).")
             return None
 
-        x = x if clf.preprocess is None else clf.preprocess.normalize(x)
+        xk = xk if clf.preprocess is None else clf.preprocess.normalize(xk)
 
         s = xs.shape[0]
-        k = x.shape[0]
+        k = xk.shape[0]
 
         Kks_ext = CArray.ones(shape=(k, s + 1))
-        Kks_ext[:, :s] = clf.kernel.k(x, xs)
+        Kks_ext[:, :s] = clf.kernel.k(xk, xs)
         return Kks_ext
 
-    def fd_x(self, x=None, y=1):
+    def fd_x(self, alpha_c, xc, xk, clf):
         """
         Derivative of the discriminant function w.r.t. an input sample
         """
-        pass
+        # handle normalizer, if present
+        xc = xc if clf.preprocess is None else clf.preprocess.normalize(xc)
+        xk = xk if clf.preprocess is None else clf.preprocess.normalize(xk)
+
+        dKkc = alpha_c * clf.kernel.gradient(xk, xc)
+        return dKkc # k * d
 
     def L_tot_d_params(self, x, y, clf):
         raise NotImplementedError()
