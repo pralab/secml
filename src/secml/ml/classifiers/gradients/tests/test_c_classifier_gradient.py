f58b5d724a3d360193f4cab22da1a96630ad7a71
Ambra <ambra.demontis@diee.unica.it>
2019-02-05 20:17:02 +0100

    add a test for the gradient of the loss function w.r.t the parameters

--- src/secml/ml/classifiers/gradients/tests/test_c_classifier_gradient.py
+++ src/secml/ml/classifiers/gradients/tests/test_c_classifier_gradient.py
@@ -37,16 +37,16 @@ class CClassifierGradientTestCases(object):
                              str(self.dataset.num_samples))
             self.logger.info("Features: %s", str(self.dataset.num_features))
 
-        def _fun_args(self, x, *args):
+        def _fun_f_args(self, x, *args):
             return self.clf.decision_function(x, **args[0])
 
-        def _grad_args(self, x, *args):
+        def _grad_f_x_args(self, x, *args):
             """
             Wrapper needed as gradient_f_x have **kwargs
             """
             return self.clf.gradient_f_x(x, **args[0])
 
-        def _clf_gradient_check(self, clf, clf_idx):
+        def _clf_gradient_f_x_check(self, clf, clf_idx):
 
             self.clf = clf
 
@@ -60,9 +60,10 @@ class CClassifierGradientTestCases(object):
                 # Compare the analytical grad with the numerical grad
                 gradient = clf.gradient_f_x(pattern, y=c)
                 num_gradient = COptimizer(
-                    CFunction(self._fun_args,
-                              self._grad_args)).approx_fprime(pattern, 1e-8,
-                                                              ({'y': c}))
+                    CFunction(self._fun_f_args,
+                              self._grad_f_x_args)).approx_fprime(pattern,
+                                                                  1e-8,
+                                                                  ({'y': c}))
                 error = (gradient - num_gradient).norm(order=1)
                 self.logger.info("Analitic gradient w.r.t. class %s: %s",
                                  str(c), str(gradient))
@@ -98,4 +99,78 @@ class CClassifierGradientTestCases(object):
                 clf = self.clf_creation_function(clf_idx, normalizer)
 
                 clf.fit(self.dataset)
-                self._clf_gradient_check(clf, clf_idx)
+                self._clf_gradient_f_x_check(clf, clf_idx)
+
+        # functionalities to test the gradient of the loss
+
+        def _fun_L_args(self, x, *args):
+            """
+            Wrapper needed as the loss function have **kwargs
+            """
+            return self.clf.gradients.L_tot(x, **args[0])
+
+        def _grad_L_params_args(self, x, *args):
+            """
+            Wrapper needed as the gradient function have **kwargs
+            """
+            return self.clf.gradients.L_tot_d_params(x, **args[0])
+
+        def _clf_gradient_L_params_check(self, clf, clf_idx):
+
+            self.clf = clf
+
+            smpls_idx = CArray.arange(self.dataset.num_samples)
+            i = self.dataset.X.randsample(smpls_idx, 1, random_state=self.seed)
+            x = self.dataset.X[i, :]
+            y = self.dataset.Y[i]
+            self.logger.info("P {:}: x {:}, y {:}".format(i, x, y))
+
+            # Compare the analytical grad with the numerical grad
+            if not hasattr(clf, 'gradients'):
+                self.logger.info("The computation of the loss fucntion "
+                                 "w.r.t. the parameter has not been "
+                                 "implmented yet for this classifier")
+                return
+
+            gradient = clf.gradients.L_tot_d_params(x, y, clf)
+            num_gradient = COptimizer(
+                CFunction(self._fun_L_args,
+                          self._grad_L_params_args)).approx_fprime(x, 1e-8, (
+                {'y': y, 'clf': clf}))
+            error = (gradient - num_gradient).norm(order=1)
+
+            self.logger.info("Analitic gradient %s", str(gradient))
+            self.logger.info("Numeric gradient %s", str(num_gradient))
+
+            self.logger.info(
+                "norm(grad - num_grad): %s", str(error))
+            self.assertLess(error, 1e-3, "problematic classifier is " +
+                            clf_idx)
+
+            for i, elm in enumerate(gradient):
+                self.assertIsInstance(elm, float)
+
+        def test_L_params_gradient(self):
+            """Test the gradient of the loss function w.r.t. the classifier
+            parameters"""
+            self.logger.info(
+                "Testing the gradient of the discriminant function")
+
+            normalizer_vals = [False, True]
+            combinations_list = [(clf_idx, normalizer) for clf_idx in \
+                                 self.clf_list for normalizer in
+                                 normalizer_vals]
+
+            for clf_idx, normalizer in combinations_list:
+                if normalizer:
+                    self.logger.info("Test the {:} classifier when it has "
+                                     "a normalizer inside ".format(clf_idx))
+                else:
+                    self.logger.info("Test the {:} classifier when it does "
+                                     "not have a normalizer inside ".format(
+                        clf_idx))
+                clf = self.clf_creation_function(clf_idx, normalizer)
+
+                clf.store_dual_vars = True
+                clf.fit(self.dataset)
+                self._clf_gradient_L_params_check(clf, clf_idx)
