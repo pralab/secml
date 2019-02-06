b5ee85fb4248cb75a6754266420b4c171fb947c4
Ambra <ambra.demontis@diee.unica.it>
2019-02-06 21:07:35 +0100

    update the test to measure the influence of the training points

--- src/secml/explanation/tests/c_explainer_local_influence_testcases.py
+++ src/secml/explanation/tests/c_explainer_local_influence_testcases.py
@@ -4,14 +4,15 @@ from secml.explanation import CExplainerLocalInfluence
 from secml.array import CArray
 from secml.data.loader import CDataLoaderMNIST
 from secml.data.splitter import CDataSplitterKFold
+from secml.ml.peval.metrics import CMetricAccuracy
+
 
 class CExplainerLocalInfluenceTestCases(CUnitTest):
     """Unittests interface for CExplainerLocalInfluence."""
 
     def _create_mnist_dataset(self, digits=[4, 9], n_tr=50, n_val=1000,
                               n_ts=1000,
-                             seed=10):
-
+                              seed=10):
         loader = CDataLoaderMNIST()
 
         tr = loader.load('training', digits=digits)
@@ -38,7 +39,6 @@ class CExplainerLocalInfluenceTestCases(CUnitTest):
         return tr, val, ts
 
     def setUp(self):
-
         self._tr, self._val, self._ts = self._create_mnist_dataset()
 
         self._clf_creation()
@@ -50,12 +50,92 @@ class CExplainerLocalInfluenceTestCases(CUnitTest):
 
         self.influences = explanation.explain(self._ts.X, self._ts.Y)
 
+    def _get_tr_without_point(self, p_idx):
+        """
+        Given the idx of a point return a copy of the training dataset
+        without that point
+
+        Parameters
+        ----------
+        p_idx int
+            idx of the point that is wanted to be excluded by the training
+            dataset
+
+        Returns
+        -------
+        new_tr CDataset
+                dataset without the point with the given index
+        """
+        all_idx = CArray.arange(self._tr.num_samples)
+        not_p_idx = all_idx.find(all_idx != p_idx)
+        new_tr = self._tr[not_p_idx, :]
+        return new_tr
+
+    def _check_influence(self, point_idx):
+        """
+        This function learn the classifier without a point and check the
+        classifier accuracy on the test set.
+
+        Parameters
+        ----------
+        point CArray
+            training sample
+        """
+        clf_copy = self._clf.deepcopy()
+        new_dataset = self._get_tr_without_point(point_idx)
+        clf_copy.fit(new_dataset)
+        preds = clf_copy.predict(self._ts.X)
+        acc = self._metric.performance_score(y_true=self._ts.Y, y_pred=preds)
+        return acc
+
+    def _check_prototype_pair(self, p_inf_idx, p_not_inf_idx):
+        """
+        Given a pair of prototypes where one is supposed to be between the
+        most influent and the one between the less influent check if this is true.
+
+        Parameters
+        ----------
+        p_inf_idx integer
+            index in the training set of a sample supposed to be between the
+            most influent
+        p_not_inf_idx integer
+            index in the training set of a sample supposed to be between the
+            less influent
+        """
+        acc_without_p_infl = self._check_influence(p_inf_idx)
+        self.logger.info("The acc without the point {:} supposed to be "
+                         "one of the most influent is {:}".format(p_inf_idx,
+                                                                  acc_without_p_infl))
+        acc_without_p_not_infl = self._check_influence(p_not_inf_idx)
+        self.logger.info("The acc without the point {:} supposed to be "
+                         "one of the less influent is {:}".format(
+            p_not_inf_idx,
+            acc_without_p_not_infl))
+
+        self.assertLess(acc_without_p_infl, acc_without_p_not_infl,
+                        "The point that is supposed to be between the "
+                        "less influent has a higher influence of the "
+                        "point supposed to be between one of the most "
+                        "influent")
 
     def _test_explanation(self):
-
         self.assertEqual(self.influences.shape,
-                         (self._ts.num_samples,self._tr.num_samples),
+                         (self._ts.num_samples, self._tr.num_samples),
                          "The shape of the influences is wrong!")
+
+        average_influence = self.influences.mean(axis=0).ravel()
+        # order the idx of the tr samples in the way to have the less
+        # influent in the first position of the vector and the most influent
+        # in the las ones
+        avg_infl_idx = average_influence.argsort()
+
+        self._metric = CMetricAccuracy()
+
+        n_check = 2
+        for i in xrange(1, n_check):
+            not_infl_idx = avg_infl_idx[i - 1].item()
+            infl_idx = avg_infl_idx[-i].item()
+            self._check_prototype_pair(infl_idx, not_infl_idx)
 
 
 if __name__ == '__main__':
