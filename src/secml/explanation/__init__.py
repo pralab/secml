401230713a2b233f3b0e1458501b7ab4d873abc6
Marco Melis <marco.melis@diee.unica.it>
2019-02-07 22:10:20 +0100

    Resolve "Implement Integrated gradients method for local explanations via relevant features"

--- src/secml/explanation/__init__.py
+++ src/secml/explanation/__init__.py
@@ -2,3 +2,5 @@ from c_explainer import CExplainer
 from c_explainer_local import CExplainerLocal
 
 from c_explainer_local_linear import CExplainerLocalLinear
+from c_explainer_local_integrated_gradients import \
+    CExplainerLocalIntegratedGradients
