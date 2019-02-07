52931d58139328473b6c482fc7f9c7e278202985
Ambra <ambra.demontis@diee.unica.it>
2019-02-07 10:32:05 +0100

    add a test when che classifier have a normalizer inside

--- src/secml/explanation/tests/test_c_explainer_local_influence_rbf_svm.py
+++ src/secml/explanation/tests/test_c_explainer_local_influence_rbf_svm.py
@@ -1,10 +1,10 @@
 from secml.explanation.tests import CExplainerLocalInfluenceTestCases
-
+from secml.ml.features.normalization import CNormalizerMinMax
 from secml.ml.classifiers import CClassifierSVM
 
 
 class TestCExplainerLocalInfluenceRbfSVM(CExplainerLocalInfluenceTestCases):
-    """Unit test for CExplainerLocalInfluenceSVM."""
+    """Unit test for CExplainerLocalInfluenceRBFSVM."""
 
     def _clf_creation(self):
         self._clf = CClassifierSVM(kernel='rbf', C=10)
@@ -14,6 +14,17 @@ class TestCExplainerLocalInfluenceRbfSVM(CExplainerLocalInfluenceTestCases):
     def test_explanation(self):
         self.logger.info("Explain the decisions of an SVM classifier and "
                          "test if they are reasonable")
+        self._test_explanation()
+
+    def test_explanation_with_normalization(self):
+        self.logger.info("Explain the decisions of an RBF-SVM classifier with "
+                         "a normalizer inside and "
+                         "test if they are reasonable")
+
+        normalizer = CNormalizerMinMax(feature_range=(-10,10))
+        normalizer.fit(self._tr.X)
+        self._clf.preprocess = normalizer
+
         self._test_explanation()
 
 
