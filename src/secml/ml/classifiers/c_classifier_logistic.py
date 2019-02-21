fd7339c6ea3df3478d91397e20c0379d597cf923
Marco Melis <marco.melis@diee.unica.it>
2019-02-21 12:07:10 +0100

    Added basic unittest for CClassifierLogistic.

--- src/secml/ml/classifiers/c_classifier_logistic.py
+++ src/secml/ml/classifiers/c_classifier_logistic.py
@@ -1,6 +1,6 @@
 """
-.. module::
-   :synopsis:
+.. module:: CClassifierLogistic
+   :synopsis: Logistic Regression (aka logit, MaxEnt) classifier
 
 .. moduleauthor:: Battista Biggio <battista.biggio@diee.unica.it>
 .. moduleauthor:: Ambra Demontis <ambra.demontis@diee.unica.it>
@@ -16,16 +16,12 @@ from secml.ml.classifiers.gradients import \
 
 
 class CClassifierLogistic(CClassifierLinear):
-    """Gradient Descent Classifier."""
+    """Logistic Regression (aka logit, MaxEnt) classifier."""
     __class_type = 'logistic'
 
     def __init__(self, C=1.0, max_iter=100, random_seed=None, preprocess=None):
 
         CClassifierLinear.__init__(self, preprocess=preprocess)
-
-        self._C = None
-        self._random_seed = None
-        self._max_iter = None
 
         self.C = C
         self.max_iter = max_iter
@@ -60,11 +56,17 @@ class CClassifierLogistic(CClassifierLinear):
 
     def _clf_init(self):
         self._sklearn_clf = LogisticRegression(
-            penalty='l2', dual=False, tol=0.0001, C=self._C,
+            penalty='l2',
+            dual=False,
+            tol=0.0001,
+            C=self._C,
             fit_intercept=True,
-            intercept_scaling=1.0, class_weight=None, solver='liblinear',
+            intercept_scaling=1.0,
+            class_weight=None,
+            solver='liblinear',
             random_state=self._random_seed,
-            max_iter=self._max_iter, multi_class='ovr',
+            max_iter=self._max_iter,
+            multi_class='ovr',
             verbose=0,
             warm_start=False,
         )
@@ -116,14 +118,14 @@ class CClassifierLogistic(CClassifierLinear):
         :return:
         """
         # Trick due to de fact that sklearn does not give to you the
-        # opportunity to fix the initial ranom weights.
+        # opportunity to fix the initial random weights.
         # (random state is used only for the training sample shuffling)
         self._sklearn_clf.max_iter = 1
         self._sklearn_clf.fit(ds.X.tondarray(), ds.Y.tondarray())
-        self._init_w = self._init_w
-        self._init_b = self._init_b
         self._sklearn_clf.max_iter = self._max_iter
         self._sklearn_clf.warm_start = True
+        self._init_w = self._init_w
+        self._init_b = self._init_b
 
     def _generate_random_params_init(self, ds):
         """
