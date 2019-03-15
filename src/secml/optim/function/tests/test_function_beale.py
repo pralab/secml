83d4b125b721d8fd944d4b81c812361180d00573
Ambra Demontis <ambra.demontis@diee.unica.it>
2019-03-15 22:24:49 +0100

    Resolve "Revise unittests of `CFunction` and subclasses by moving few test cases methods to a new CFunctionTestCases class"

--- src/secml/optim/function/tests/test_function_beale.py
+++ src/secml/optim/function/tests/test_function_beale.py
@@ -1,50 +1,26 @@
-import unittest
-from secml.utils import CUnitTest
+from secml.optim.function.tests import CFunctionTestCases
 
-from secml.figure import CFigure
 from secml.array import CArray
 from secml.optim.function import CFunction
-from secml.core.type_utils import is_scalar
 
 
-class TestCFunctionBeale(CUnitTest):
+class TestCFunctionBeale(CFunctionTestCases):
     """Unit test for CFunctionBeale."""
 
     def setUp(self):
+        self.fun = CFunction.create('beale')
 
-        self.fun_obj = CFunction.create('beale')
-
-        self.logger.info("Global minimum: {:}".format(
-            self.fun_obj.global_min()))
-        self.logger.info("Global minimum @: {:}".format(
-            self.fun_obj.global_min_x()))
-
-    def test_function_result(self):
+    def test_fun_result(self):
         """Test if function returns the correct value."""
-        x = CArray([3, 0.5])
-
-        funct_res = self.fun_obj.fun(x)
-
-        self.logger.info(
-            "Correct result: {:}".format(self.fun_obj.global_min()))
-        self.logger.info("Function result: {:}".format(funct_res))
-
-        self.assertTrue(is_scalar(funct_res))
-        self.assertAlmostEqual(self.fun_obj.global_min(), funct_res, places=4)
+        self._show_global_min(self.fun)
+        self._test_fun_result(
+            self.fun, CArray([3, 0.5]), self.fun.global_min())
 
     def test_2D(self):
         """Plot of a 2D example."""
         grid_limits = [(-4.5, 4.5), (-4.5, 4.5)]
-
-        fig = CFigure(width=7)
-        fig.switch_sptype(sp_type='function')
-        fig.sp.plot_fobj(func=self.fun_obj.fun, plot_levels=True,
-                         grid_limits=grid_limits, levels=[1],
-                         n_grid_points=50, n_colors=200, vmin=0, vmax=5)
-
-        fig.sp.title("Beale Function")
-        fig.show()
+        self._test_2D(self.fun, grid_limits, levels=[1], vmin=0, vmax=5)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    CFunctionTestCases.main()
