b7b990ff401ef785d98bcd8ba96b1a8850db2876
Ambra Demontis <ambra.demontis@diee.unica.it>
2019-03-20 18:57:02 +0100

    Resolve "Implement tests to check if the optimizers work correctly in discrete problems"

---
+++ src/secml/optim/optimizers/tests/c_optimizer_testcases.py
@@ -0,0 +1,163 @@
+from secml.utils import CUnitTest
+
+from secml.array import CArray
+from secml.optim.function import CFunction
+from secml.optim.constraints import CConstraintBox
+from secml.figure import CFigure
+import secml.utils.c_file_manager as fm
+
+
+class COptimizerTestCases(CUnitTest):
+    """Unittests interface for COptimizer."""
+
+    def setUp(self):
+
+        self.test_funcs = dict()
+
+        # Instancing the available functions to test optimizer
+        self.test_funcs['3h-camel'] = {
+            'fun': CFunction.create('3h-camel'),
+            'x0': CArray([2, 2]),
+            'grid_limits': [(-2, 2), (-2, 2)],
+            'vmin': 0, 'vmax': 5
+        }
+        self.test_funcs['beale'] = {
+            'fun': CFunction.create('beale'),
+            'x0': CArray([0, 0]),
+            'grid_limits': [(-1, 4.5), (-1, 1.5)],
+            'vmin': 0, 'vmax': 5
+        }
+        self.test_funcs['mc-cormick'] = {
+            'fun': CFunction.create('mc-cormick'),
+            'x0': CArray([2, 0]),
+            'grid_limits': [(-2, 3), (-3, 1)],
+            'vmin': -2, 'vmax': 2
+        }
+        self.test_funcs['rosenbrock'] = {
+            'fun': CFunction.create('rosenbrock'),
+            'x0': CArray([-1, -1]),
+            'grid_limits': [(-1.1, 1.1), (-1.1, 1.1)],
+            'vmin': 0, 'vmax': 10
+        }
+        quad = self._create_quadratic_fun()
+        self.test_funcs['quadratic'] = {
+            'fun': quad,
+            'x0': CArray([4., -4.]),
+            'grid_limits': [(-5, 5), (-5, 5)],
+            'vmin': None, 'vmax': None
+        }
+
+    def _create_quadratic_fun(self):
+        """Creates a quadratic function."""
+        A = CArray.eye(2, 2)
+        b = CArray.zeros((2, 1))
+        c = 0
+
+        discr_fun = CFunction.create('quadratic', A, b, c)
+        discr_fun.global_min = lambda: 0.
+        discr_fun.global_min_x = lambda: CArray([0, 0])
+
+        return discr_fun
+
+    def _test_minimize(
+            self, opt_class, fun_id, opt_params=None, minimize_params=None):
+        """Test for COptimizer.minimize() method.
+
+        Parameters
+        ----------
+        opt_class : class
+            COptimizer.
+        fun_id : str
+            Id of the function to test. Should be available in "test_funcs"
+            dictionary (see setUp).
+        opt_params : dict or None, optional
+            Dictionary of parameters for the optimizer.
+        minimize_params : dict or None, optional
+            Dictionary of parameters for the minimize method.
+
+        """
+        opt_params = {} if opt_params is None else opt_params
+        minimize_params = {} if minimize_params is None else minimize_params
+
+        fun_dict = self.test_funcs[fun_id]
+
+        fun = fun_dict['fun']
+
+        opt = opt_class(fun, **opt_params)
+        opt.verbose = 1
+
+        self.logger.info(
+            "Testing minimization of {:} using {:}".format(
+                fun.__class__.__name__, opt.__class__.__name__))
+
+        if fun.class_type == 'mc-cormick' and 'bounds' not in opt_params:
+            # set bounds
+            lb, ub = fun.bounds()
+            opt.bounds = CConstraintBox(lb, ub)
+
+        min_x = opt.minimize(fun_dict['x0'], **minimize_params)
+
+        self.logger.info("Found minimum: {:}".format(min_x))
+        self.logger.info("Fun value @ minimum: {:}".format(opt.f_opt))
+
+        self._plot_optimization(opt, fun_dict['x0'], min_x,
+                                grid_limits=fun_dict['grid_limits'],
+                                vmin=fun_dict['vmin'],
+                                vmax=fun_dict['vmax'])
+
+        # Round results for easier asserts
+        self.assertAlmostEqual(opt.f_opt, fun.global_min(), places=4)
+        self.assert_array_almost_equal(
+            min_x, fun.global_min_x(), decimal=4)
+
+    def _plot_optimization(
+            self, solver, x_0, g_min, grid_limits, vmin=None, vmax=None):
+        """Plots the optimization problem.
+
+        Parameters
+        ----------
+        solver : COptimizer
+        x_0 : CArray
+            Starting point.
+        g_min : CArray
+            Final point (after optimization).
+        grid_limits : list of tuple
+        vmin, vmax : int or None, optional
+
+        """
+        fig = CFigure(markersize=12)
+        fig.switch_sptype(sp_type='function')
+
+        # Plot objective function
+        fig.sp.plot_fobj(func=CArray.apply_along_axis,
+                         plot_background=True,
+                         n_grid_points=30, n_colors=25,
+                         grid_limits=grid_limits,
+                         levels=[1], levels_color='gray', levels_style='--',
+                         colorbar=True, func_args=(solver.f.fun, 1,),
+                         vmin=vmin, vmax=vmax)
+
+        if solver.bounds is not None:  # Plot box constraint
+            fig.sp.plot_fobj(func=lambda x: solver.bounds.constraint(x),
+                             plot_background=False, n_grid_points=20,
+                             grid_limits=grid_limits, levels=[0],
+                             colorbar=False)
+
+        if solver.constr is not None:  # Plot distance constraint
+            fig.sp.plot_fobj(func=lambda x: solver.constr.constraint(x),
+                             plot_background=False, n_grid_points=20,
+                             grid_limits=grid_limits, levels=[0],
+                             colorbar=False)
+
+        # Plot optimization trace
+        if solver.x_seq is not None:
+            fig.sp.plot_path(solver.x_seq)
+        else:
+            fig.sp.plot_path(x_0.append(g_min, axis=0))
+
+        fig.sp.title("{:}(fun={:}) - Glob Min @ {:}".format(
+            solver.class_type,  solver.f.class_type,
+            solver.f.global_min_x().tolist()))
+        fig.savefig(
+            fm.join(fm.abspath(__file__),
+                    solver.class_type + '_' + solver.f.class_type + '.pdf'))
--- src/secml/optim/optimizers/tests/test_c_optimizer_scipy.py
+++ src/secml/optim/optimizers/tests/test_c_optimizer_scipy.py
@@ -1,148 +1,25 @@
-from secml.utils import CUnitTest
+from secml.optim.optimizers.tests import COptimizerTestCases
 
-from secml.array import CArray
 from secml.optim.optimizers import COptimizerScipy
-from secml.optim.function import CFunction
-from secml.optim.constraints import CConstraintBox
 
 
-class TestCOptimizer(CUnitTest):
-    """Test for COptimizer class."""
-
-    def setUp(self):
-
-        avail_funcs = ['3h-camel', 'beale', 'mc-cormick', 'rosenbrock']
-
-        # Instancing the available functions to test optimizer
-        self.funcs = {}
-        for fun_id in avail_funcs:
-            self.funcs[fun_id] = CFunction.create(fun_id)
+class TestCOptimizerScipy(COptimizerTestCases):
+    """Unittests for COptimizerScipy."""
 
     def test_minimize(self):
         """Test for COptimizer.minimize() method."""
-        self.logger.info("Test for COptimizer.minimize() method.")
 
-        x0 = CArray([1., 0.])  # Starting point for minimization
+        minimize_params = {'method': 'L-BFGS-B', 'options': {'gtol': 1e-6}}
 
-        for fun_id in self.funcs:
-            fun = self.funcs[fun_id]
-
-            self.logger.info(
-                "Testing minimization of {:}".format(fun.__class__.__name__))
-
-            opt = COptimizerScipy(fun)
-            opt.verbose = 1
-
-            if fun.class_type == 'mc-cormick':
-                # set bounds
-                lb, ub = fun.bounds()
-                opt.bounds = CConstraintBox(lb, ub)
-
-            min_x = opt.minimize(
-                x0, method='L-BFGS-B', options={'gtol': 1e-6})
-
-            self.logger.info("Found minimum: {:}".format(min_x))
-            self.logger.info("Fun value @ minimum: {:}".format(opt.f_opt))
-
-            # Round results for easier asserts
-            self.assertAlmostEqual(
-                round(opt.f_opt, 4), fun.global_min(), places=4)
-            self.assertFalse(
-                (min_x.round(decimals=4) != fun.global_min_x()).any())
-
-    def test_approx_fprime_check_grad(self):
-        """Test if the gradient check made up with
-        COptimizer.approx_fprime() and .check_grad() methods is correct."""
-        self.logger.info(
-            "Test for COptimizer.approx_fprime() and .check_grad() methods.")
-
-        x0 = CArray([1., 0.])  # Starting point for minimization
-
-        for fun_id in self.funcs:
-            fun = self.funcs[fun_id]
-
-            self.logger.info(
-                "Testing grad approx of {:}".format(fun.__class__.__name__))
-
-            grad_err = fun.check_grad(x0)
-
-            self.logger.info(
-                "(Real grad - approx).norm(): {:}".format(grad_err))
-
-            self.assertLess(grad_err, 1e-3)
-
-    # Two dumb function with two required parameters
-    def _fun_2_params(self, x, y):
-        return 1
-
-    def _dfun_2_params(self, x, y):
-        return CArray([0])
-
-    # Two dumb function that have **kwargs as the second parameter
-    def _fun_kwargs(self, x, **kwargs):
-        if kwargs != {'y': 1}:
-            raise ValueError
-        return 1
-
-    def _dfun_kwargs(self, x, **kwargs):
-        if kwargs != {'y': 1}:
-            raise ValueError
-        return CArray([0])
-
-    def _fun_args(self, x, *args):
-        return self._fun_kwargs(x, **args[0])
-
-    def _dfun_args(self, x, *args):
-        return self._dfun_kwargs(x, **args[0])
-
-    def test_approx_fprime_check_param_passage(self):
-        """Test the functions COptimizer.approx_fprime() and .check_grad()
-        are correctly passing the extra parameters to the main function and
-        the one that computes the gradient.
-
-        """
-        self.logger.info(
-            "Test the parameters passage made up by "
-            "COptimizer.approx_fprime() "
-            "and .check_grad() methods.")
-
-        x0 = CArray([1.])  # Starting point for minimization
-        epsilon = 0.1
-
-        self.logger.info(
-            "Testing when the function and the gradient have two parameter")
-
-        fun = CFunction(fun=self._fun_2_params, gradient=self._dfun_2_params)
-        self.logger.info("Testing check_grad")
-
-        grad_err = fun.check_grad(x0, 1)
-        self.logger.info("Grad error: {:}".format(grad_err))
-        self.assertEqual(0, grad_err)
-
-        self.logger.info("Testing approx_fprime")
-
-        grad_err = fun.approx_fprime(x0, epsilon, 1).item()
-        self.logger.info("Grad error: {:}".format(grad_err))
-        self.assertEqual(0, grad_err)
-
-        self.logger.info(
-            "Testing when the function and the gradient have **kwargs "
-            " as second parameter")
-
-        fun = CFunction(fun=self._fun_args, gradient=self._dfun_args)
-
-        self.logger.info("Testing check_grad ")
-
-        grad_err = fun.check_grad(x0, {'y': 1})
-        self.logger.info("Grad error: {:}".format(grad_err))
-        self.assertEqual(0, grad_err)
-
-        self.logger.info("Testing approx_fprime ")
-
-        grad_err = fun.approx_fprime(x0, epsilon, ({'y': 1}))
-        self.logger.info("Grad error: {:}".format(grad_err))
-        self.assertEqual(0, grad_err)
+        self._test_minimize(
+            COptimizerScipy, '3h-camel', minimize_params=minimize_params)
+        self._test_minimize(
+            COptimizerScipy, 'beale', minimize_params=minimize_params)
+        self._test_minimize(
+            COptimizerScipy, 'mc-cormick', minimize_params=minimize_params)
+        self._test_minimize(
+            COptimizerScipy, 'rosenbrock', minimize_params=minimize_params)
 
 
 if __name__ == "__main__":
-    CUnitTest.main()
+    COptimizerTestCases.main()
