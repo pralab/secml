586171e9a6ddea882b241219c242f4faf624dbee
Ambra <ambra.demontis@diee.unica.it>
2019-03-21 16:35:22 +0100

    simplify the box constraint test

--- src/secml/optim/constraints/tests/test_c_constraint_box.py
+++ src/secml/optim/constraints/tests/test_c_constraint_box.py
@@ -1,62 +1,25 @@
-"""
-Created on 27/apr/2015
-This class implements tests for the CConstraintBox
-@author: Davide Maiorca
-
-"""
-import unittest
 from secml.utils import CUnitTest
+from test_c_constraint import CConstraintTestCases
 
-from secml.data import CDataset
-from secml.data.loader import CDataLoader
-from secml.array import CArray
-from secml.optim.constraints import CConstraint
 from secml.optim.constraints import CConstraintBox
-from secml.figure import CFigure
+from secml.core.constants import inf
 
+class TestConstraintBox(CConstraintTestCases.TestCConstraint):
+    """Test the L-inf distance constraint."""
 
-class TestCConstraintBox(CUnitTest):
-    """Unit test for CConstraintBox."""
+    def _constr_creation(self):
+        lb = -1
+        ub = 1
+        self._constr = CConstraintBox(lb=lb, ub=ub)
 
-    def setUp(self):
-        """Sets up the test."""
-        self.logger.info("Generating training set... ")
-        loader = CDataLoader.create('blobs')
-        self.dataset = loader.load()
-        # self.lb = -1
-        self.lb = [-1, -0.5]
-        # self.ub = 2
-        self.ub = [1, 2]
-        self.constraint = CConstraintBox(lb=self.lb, ub=self.ub)
+    def _set_constr_name(self):
+        self._constr_name = 'L-inf'
 
-    def test_instantiation(self):
-        """Test constraint instantiation."""
-        self.logger.info("Testing CConstraintBox instantiation method")
-        self.assertEqual(CConstraint.create('box', self.lb, self.ub).__class__.__name__,
-                         self.constraint.__class__.__name__)
-
-#     #FIXME: Implement meaningful tests
-#     def test_projection(self):
-#         """Test projection onto feasible box."""
-#         self.logger.info("Testing projection on box")
-#         print self.dataset.X[0]
-#         print self.constraint.constraint(self.dataset.X)
-#         print self.constraint.projection(self.dataset.X[0])
-#         print self.constraint.constraint(self.dataset.X[0])
-
-    def test_box_plot(self):
-        grid_limits = [(-3, 3), (-3, 3)]
-        fig = CFigure(height=6, width=6)
-        fig.switch_sptype(sp_type='function')
-        fig.sp.plot_fobj(func=self.constraint.constraint,
-                         grid_limits=grid_limits)
-        fig.sp.plot_fobj(func=self.constraint.constraint,
-                         plot_background=False,
-                         n_grid_points=50,
-                         grid_limits=grid_limits,
-                         levels=[0])
-        fig.show()
-
+    def _set_norm_order(self):
+        """
+        Set the distance on which the constrain is computed
+        """
+        self._norm_order = inf
 
 if __name__ == '__main__':
-    unittest.main()
+    CUnitTest.main()
