f9db2964b9d683691bea3b9a1d163b09b66a6a8c
Marco Melis <marco.melis@diee.unica.it>
2019-03-26 12:18:29 +0100

    #198 Updated documentation to include reference to the supported Python 3 versions.

--- setup.py
+++ setup.py
@@ -133,6 +133,10 @@ License :: OSI Approved
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
 Programming Language :: Python :: Implementation :: PyPy
 Topic :: Software Development
 Topic :: Scientific/Engineering
