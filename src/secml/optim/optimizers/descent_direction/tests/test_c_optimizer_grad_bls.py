7a55bf6a9c05cdadecd0e0926d2e1aa0bedbaa28
Ambra <ambra.demontis@diee.unica.it>
2019-04-23 17:49:06 +0200

    cleaning (remove discrete tests from here)

--- src/secml/optim/optimizers/descent_direction/tests/test_c_optimizer_grad_bls.py
+++ src/secml/optim/optimizers/descent_direction/tests/test_c_optimizer_grad_bls.py
@@ -14,30 +14,12 @@ class TestCOptimizerGradBLS(COptimizerTestCases):
         self._test_minimize(
             COptimizerGradBLS, '3h-camel', opt_params=opt_params)
 
-        # Test discrete optimization with float eta
-        opt_params = {'eta': 0.5, 'eta_min': 0.5, 'eps': 1e-12,
-                      'discrete': True,
-                      'bounds': CConstraintBox(lb=-1, ub=1)}
-
-        self._test_minimize(COptimizerGradBLS, '3h-camel',
-                            opt_params=opt_params,
-                            label='discrete')
-
     def test_minimize_beale(self):
         """Test for COptimizer.minimize() method on beale fun."""
         opt_params = {'eta': 1e-6, 'eta_min': 1e-4, 'eps': 1e-12}
 
         self._test_minimize(
             COptimizerGradBLS, 'beale', opt_params=opt_params)
-
-        # Test discrete optimization with float eta
-        opt_params = {'eta': 1e-6, 'eta_min': 1e-4, 'eps': 1e-12,
-                      'discrete': True,
-                      'bounds': CConstraintBox(lb=0, ub=4)}
-
-        self._test_minimize(COptimizerGradBLS, 'beale',
-                            opt_params=opt_params,
-                            label='discrete')
 
     def test_minimize_mc_cormick(self):
         """Test for COptimizer.minimize() method on mc-cormick fun."""
@@ -52,67 +34,6 @@ class TestCOptimizerGradBLS(COptimizerTestCases):
 
         self._test_minimize(
             COptimizerGradBLS, 'rosenbrock', opt_params=opt_params)
-
-    def test_minimize_discrete(self):
-        """Test for COptimizer.minimize() method in discrete space."""
-
-        # test a simple function without any bound
-        opt_params = {'eta': 1, 'eta_min': 1, 'eps': 1e-12,
-                      'discrete': True}
-
-        # both the starting point and eta are integer,
-        # therefore we expect an integer solution
-        self._test_minimize(COptimizerGradBLS, 'quad-2',
-                            opt_params=opt_params,
-                            label='quad-2-discrete',
-                            out_int=True)
-
-        # Testing bounded optimization
-        opt_params = {'eta': 1, 'eta_min': 1, 'eps': 1e-12,
-                      'discrete': True,
-                      'bounds': CConstraintBox(lb=-2, ub=3)}
-
-        self._test_minimize(
-            COptimizerGradBLS, 'quad-2',
-            opt_params=opt_params,
-            label='quad-2-discrete-bounded',
-            out_int=True)
-
-        self._test_minimize(
-            COptimizerGradBLS, 'quad-100-sparse',
-            opt_params=opt_params,
-            label='quad-100-sparse-discrete-bounded',
-            out_int=True)
-
-        # Testing bounded optimization
-        opt_params = {'eta': 1, 'eta_min': 1, 'eps': 1e-12,
-                      'discrete': True,
-                      'bounds': CConstraintBox(lb=-1, ub=1)}
-
-        self._test_minimize(
-            COptimizerGradBLS, 'exp-sum-2',
-            opt_params=opt_params,
-            label='exp-sum-discrete-bounded',
-        )
-
-        self._test_minimize(
-            COptimizerGradBLS, 'exp-sum-100-int',
-            opt_params=opt_params,
-            label='exp-sum-int-discrete-bounded',
-            out_int=True)
-
-        self._test_minimize(
-            COptimizerGradBLS, 'exp-sum-100-int-sparse',
-            opt_params=opt_params,
-            label='exp-sum-int-sparse-discrete-bounded',
-            out_int=True)
-
-        # Discrete optimization + L2 constraint is not supported
-        with self.assertRaises(NotImplementedError):
-            opt_params = {'eta': 1e-6, 'eta_min': 1e-4, 'eps': 1e-12,
-                          'discrete': True, 'constr': CConstraintL2()}
-            self._test_minimize(
-                COptimizerGradBLS, 'beale', opt_params=opt_params)
 
 
 if __name__ == '__main__':
