f7d35fadcbd0b1ffdbc2ec6dc05995bf9bcd0d54
Marco Melis <marco.melis@diee.unica.it>
2019-07-10 19:54:24 +0200

    #472 Minimum required version of `tensorflow` is now `1.14` in order to get latest updates and fix few `PendingDeprecationWarnings`.

--- setup.py
+++ setup.py
@@ -166,8 +166,8 @@ setup(
     install_requires=REQ_PKGS,
     extras_require={
         'pytorch': ["torch>=0.4", "torchvision>=0.1.8"],
-        'cleverhans': ["tensorflow>=1.13,<2", "cleverhans"],
-        'tf-gpu': ["tensorflow-gpu>=1.13,<2"],
+        'cleverhans': ["tensorflow>=1.14,<2", "cleverhans"],
+        'tf-gpu': ["tensorflow-gpu>=1.14,<2"],
         'unittests': ['pytest>=4.2', 'pytest-cov>=2.6.1']
     },
     zip_safe=False
