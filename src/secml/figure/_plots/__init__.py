9a1d7d7aed95259982cf66acee5833bc84102528
bat <battista.biggio@diee.unica.it>
2019-07-25 17:23:42 +0200

    Added plot constraints.

--- src/secml/figure/_plots/__init__.py
+++ src/secml/figure/_plots/__init__.py
@@ -5,3 +5,4 @@ from .c_plot_ds import CPlotDataset
 from .c_plot_clf import CPlotClassifier
 from .c_plot_sec_eval import CPlotSecEval
 from .c_plot_stats import CPlotStats
+from .c_plot_constraint import CPlotConstraint
