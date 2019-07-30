d0b8b1d422355aedaf064479276f3355c95e3b69
bat <battista.biggio@diee.unica.it>
2019-07-30 11:37:55 +0200

    Now tests also run for 2-class datasets. However, the test principle is essentially wrong here. We are comparing decision_function and _decision_function which are essentially the same function. In fact, I'm reimplementing the same function from CClassifierSkLearn in the tests. We may need to think a different testing strategy here. Overall, it seems that the wrapper is working properly.

--- src/secml/ml/classifiers/tests/test_c_classifier_sklearn.py
+++ src/secml/ml/classifiers/tests/test_c_classifier_sklearn.py
@@ -1,6 +1,6 @@
 from secml.ml.classifiers.tests import CClassifierTestCases
 
-from secml.data.loader import CDLIris
+from secml.data.loader import CDLIris, CDLRandom
 from secml.ml.classifiers import CClassifierSkLearn
 from secml.array import CArray
 
@@ -21,15 +21,21 @@ class TestCClassifierSkLearn(CClassifierTestCases):
 
     def setUp(self):
         self.dataset = CDLIris().load()
+        # self.dataset = CDLRandom(
+        #     n_features=100, n_redundant=20, n_informative=25,
+        #     n_clusters_per_class=2, random_state=0).load()
 
         self.skclfs = [
             KNeighborsClassifier(3),
-            SVC(kernel="linear", C=0.025, decision_function_shape='ovr'),
-            SVC(kernel="rbf", gamma=2, C=1),
+            SVC(kernel="linear", C=0.025,
+                random_state=0, decision_function_shape='ovr'),
+            SVC(kernel="rbf", gamma=2, C=1, random_state=0),
             DecisionTreeClassifier(max_depth=5, random_state=0),
-            RandomForestClassifier(max_depth=5, n_estimators=5, random_state=0),
+            RandomForestClassifier(max_depth=5, n_estimators=5,
+                                   random_state=0),
             MLPClassifier(alpha=1, max_iter=1000, random_state=0),
             AdaBoostClassifier(random_state=0),
+            OneVsRestClassifier(SVC()),
             # These clf below only work on dense data!
             GaussianProcessClassifier(1.0 * RBF(1.0)),
             GaussianNB(),
@@ -45,15 +51,30 @@ class TestCClassifierSkLearn(CClassifierTestCases):
 
         for i, clf in enumerate(self.classifiers):
 
-            # create a fake private decision_function to run tests
+            print(clf._sklearn_model)
+
+            # create a fake private _decision_function to run tests
+            # but this is basically the same in CClassifierSkLearn
+            # - we need to think better tests!
             def _decision_function(x, y):
                 x = x.atleast_2d()
                 try:
                     scores = CArray(
                         self.skclfs[i].decision_function(x.get_data()))
+                    probs = False
                 except AttributeError:
                     scores = CArray(self.skclfs[i].predict_proba(x.get_data()))
+                    probs = True
+
+                # two-class classifiers outputting only scores for class 1
+                if len(scores.shape) == 1:  # duplicate column for class 0
+                    outputs = CArray.ones(shape=(x.shape[0], clf.n_classes))
+                    outputs[:, 1] = scores.T
+                    outputs[:, 0] = -scores.T if probs is False else 1 - scores.T
+                    scores = outputs
+                scores.atleast_2d()
                 return scores[:, y].ravel()
+
             clf._decision_function = _decision_function
 
             # execute tests
