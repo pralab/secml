fix: gradient of layer unit wrt input

src/secml/ml/classifiers/c_classifier_pytorch.py
@@ -204,6 +204,13 @@ class CClassifierPyTorch(CClassifier, CClassifierGradientPyTorchMixin):
         else:
             raise TypeError("The input model must be an instance of `nn.Module`.")
 
+    def get_layer_shape(self, layer_name):
+        layer = next(filter(lambda x: x[0] == layer_name, get_layers(self._model)))[1]
+        if isinstance(layer, nn.Linear):
+            return (1, layer.out_features)
+        else:
+            raise ValueError("Layer output shape can be requested only for linear layers.")
+
     def _clean_hooks(self):
         """Removes previously defined hooks."""
         for handler in self._handlers:
@@ -533,7 +540,7 @@ class CClassifierPyTorch(CClassifier, CClassifierGradientPyTorchMixin):
         ----------
         s : torch.Tensor
             Input tensor to forward propagate.
-        layer_names : list, str or None, optional
+        layer_names : list or None, optional
             Name of the layer(s) to hook for getting the output.
             If None, the output of the last layer will be returned.
 
@@ -545,14 +552,18 @@ class CClassifierPyTorch(CClassifier, CClassifierGradientPyTorchMixin):
         """
         # Switch to evaluation mode
         self._model.eval()
-
         if layer_names is None:  # Directly use the last layer
             return self._model(s)  # Forward pass
 
-        else:
+        elif isinstance(layer_names, list) or isinstance(layer_names, str):
+            if isinstance(layer_names, str):
+                layer_names = [layer_names]
+
             self.hook_layer_output(layer_names)
             self._model(s)
             return {layer_names[i]: v for i, (k, v) in enumerate(self._intermediate_outputs.items())}
+        else:
+            raise ValueError("Pass layer names as a list or just None for last layer output.")
 
     def save_checkpoint(self):
         pass
@@ -570,7 +581,6 @@ class CClassifierPyTorch(CClassifier, CClassifierGradientPyTorchMixin):
         state = {
             'model_state': self._model.state_dict(),
             'optimizer_state': self._optimizer.state_dict(),
-            'loss': self._loss,
             'n_features': self.n_features,
             'classes': self.classes,
         }
@@ -593,6 +603,5 @@ class CClassifierPyTorch(CClassifier, CClassifierGradientPyTorchMixin):
 
         self._model.load_state_dict(state['model_state'])
         self._optimizer.load_state_dict(state['optimizer_state'])
-        self._loss = state['loss']
         self._n_features = state['n_features']
         self._classes = state['classes']

src/secml/ml/classifiers/gradients/mixin_classifier_gradient_pytorch.py
@@ -14,6 +14,7 @@ from secml.ml.classifiers.gradients import CClassifierGradientMixin
 from secml.ml.classifiers.loss import CSoftmax
 
 from secml.settings import SECML_PYTORCH_USE_CUDA
+
 use_cuda = torch.cuda.is_available() and SECML_PYTORCH_USE_CUDA
 
 
@@ -100,35 +101,30 @@ class CClassifierGradientPyTorchMixin(CClassifierGradientMixin):
         # keep track of the gradient in s tensor
         s.requires_grad = True
 
+        # make sure the gradient is empty
+        if s.grad is not None:
+            s.grad = None
+
         # Get the model output at specific layer
-        layer_outputs = self._get_layer_output(s, layer_names=layer)
+        layer_output = self._get_layer_output(s, layer_names=layer)
+        if isinstance(layer_output, dict):
+            layer_output = layer_output[layer]
 
-        if isinstance(layer_outputs, dict):
-            for layer_name, out in layer_outputs.items():
-
-                # unsqueeze if net output does not take into account the batch size
-                if len(out.shape) < len(s.shape):
-                    out = out.unsqueeze(0)
-
-                w = self._to_tensor(w).reshape(out.shape)
-                if s.grad is not None:
-                    s.grad = None
-                out.backward(w)
-
-        else:
-            w = torch.zeros(layer_outputs.shape)
-            w[y] = 1
+        if w is not None and y is None:
+            w = self._to_tensor(w.atleast_2d())
+        elif y is not None and w is None:
+            w = torch.zeros(layer_output.shape)
+            w[:, y] = 1
             # Apply softmax-scaling if needed
             if self.softmax_outputs is True:
-                out_carray = self._from_tensor(layer_outputs.squeeze(0).data)
+                out_carray = self._from_tensor(layer_output.squeeze(0).data)
                 softmax_grad = CSoftmax().gradient(out_carray, y=y)
-                layer_outputs *= self._to_tensor(softmax_grad.atleast_2d()).unsqueeze(0)
+                layer_output *= self._to_tensor(softmax_grad.atleast_2d()).unsqueeze(0)
+        else:
+            # both `w` and `y` are passed or none of them
+            raise ValueError("Either `w` or `y` must be passed.")
 
-
-            if s.grad is not None:
-                s.grad = None
-
-            layer_outputs.backward(w)
-
+        print(layer_output.shape)
+        layer_output.backward(w)
 
         return self._from_tensor(s.grad.data.view(-1))