419c0275e67d6c0542520d4e2fbcb42510389790
Marco Melis <marco.melis@unica.it>
2020-02-17 15:30:57 +0100

    #721 Dictionary with models zoo definition is now dynamically downloaded and updated from our repository located at https://gitlab.com/secml/secml-zoo.

--- src/secml/ml/model_zoo/load_model.py
+++ src/secml/ml/model_zoo/load_model.py
@@ -15,9 +15,75 @@ from secml.utils.download_utils import dl_file_gitlab
 from secml.settings import SECML_MODELS_DIR
 
 MODEL_ZOO_REPO_URL = 'https://gitlab.com/secml/secml-zoo'
-MODELS_DICT_PATH = fm.join(fm.abspath(__file__), 'models_dict.json')
-with open(MODELS_DICT_PATH) as fp:
-    MODELS_DICT = json.loads(fp.read())
+MODELS_DICT_FILE = 'models_dict.json'
+MODELS_DICT_PATH = fm.join(SECML_MODELS_DIR, MODELS_DICT_FILE)
+
+
+def _dl_data_versioned(file_path, output_dir, md5_digest=None):
+    """Download the from different branches depending on version.
+
+    This function tries to download a model zoo resource from:
+     1. the branch corresponding to current version,
+        e.g. branch `v0.12` for `0.12.*` version
+     2. the `master` branch
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the file to download, relative to the repository.
+    output_dir : str
+        Path to the directory where the file should be stored.
+        If folder does not exists, will be created.
+    md5_digest : str or None, optional
+        Expected MD5 digest of the downloaded file.
+        If a different digest is computed, the downloaded file will be
+        removed and ValueError is raised.
+
+    """
+    try:
+        # Try downloading from the branch corresponding to current version
+        min_version = re.search(r'^\d+.\d+', secml.__version__).group(0)
+        dl_file_gitlab(MODEL_ZOO_REPO_URL, file_path, output_dir,
+                       branch='v' + min_version, md5_digest=md5_digest)
+
+    except RuntimeError:
+        # Raised if file not found. Try looking in 'master' branch
+        dl_file_gitlab(MODEL_ZOO_REPO_URL, file_path, output_dir,
+                       branch='master', md5_digest=md5_digest)
+
+
+def _get_models_dict():
+    """Downloads the ditionary of models definitions.
+
+    Returns
+    -------
+    models_dict : dict
+        Dictionary with models definitions. Each key is an available model.
+        Each model entry is defined by:
+         - "model", path to the script with model definition
+         - "state", path to the archive containing the pre-saved model state
+         - "model_md5", md5 checksum of model definition
+         - "state_md5", md5 checksum of pre-saved model state
+
+    """
+    # Download (if needed) data and extract it
+    if not fm.file_exist(MODELS_DICT_PATH):
+
+        # Download definitions from current version's branch first,
+        # then from master branch
+        _dl_data_versioned(MODELS_DICT_FILE, SECML_MODELS_DIR)
+
+        # Check if file has been correctly downloaded
+        if not fm.file_exist(MODELS_DICT_PATH):
+            raise RuntimeError(
+                'Something wrong happened while downloading the '
+                'models definitions. Please try again.')
+
+    with open(MODELS_DICT_PATH) as fp:
+        return json.loads(fp.read())
+
+
+MODELS_DICT = _get_models_dict()  # Populate the models dict
 
 
 def load_model(model_id):
@@ -25,15 +91,7 @@ def load_model(model_id):
 
     Returns a pre-trained SecML classifier given the id of the model.
 
-    The following models are available:
-     - `mnist-svm`,
-            multiclass `CClassifierSVM` trained on MNIST
-     - `mnist59-svm`,
-            multiclass `CClassifierSVM` with RBF Kernel trained on MNIST59
-     - `mnist59-svm-rbf`,
-            multiclass `CClassifierSVM` with RBF Kernel trained on MNIST59
-     - `mnist159-cnn`,
-            `CClassifierPyTorch` CNN trained on MNIST159
+    Check https://gitlab.com/secml/secml-zoo for the list of available models.
 
     Parameters
     ----------
@@ -50,25 +108,16 @@ def load_model(model_id):
     data_path = fm.join(SECML_MODELS_DIR, model_id, model_id + '.gz')
     # Download (if needed) data and extract it
     if not fm.file_exist(data_path):
-        model_url = 'models/' + model_info['url'] + '.gz'
+        model_url = 'models/' + model_info['state'] + '.gz'
         out_dir = fm.join(SECML_MODELS_DIR, model_id)
-        try:
-            # Try downloading from the branch corresponding to current version
-            min_version = re.search(r'^\d+.\d+', secml.__version__).group(0)
-            dl_file_gitlab(MODEL_ZOO_REPO_URL, model_url, out_dir,
-                           branch='v' + min_version,
-                           md5_digest=model_info['md5'])
-
-        except RuntimeError:
-            # Raised if file not found. Try looking in 'master' branch
-            dl_file_gitlab(MODEL_ZOO_REPO_URL, model_url, out_dir,
-                           branch='master',
-                           md5_digest=model_info['md5'])
+        # Download requested model from current version's branch first,
+        # then from master branch
+        _dl_data_versioned(model_url, out_dir, model_info['state_md5'])
 
         # Check if file has been correctly downloaded
         if not fm.file_exist(data_path):
             raise RuntimeError('Something wrong happened while '
-                               'downloading the model. Please try again')
+                               'downloading the model. Please try again.')
 
     def import_module(full_name, path):
         """Import a python module from a path."""
