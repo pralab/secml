ac288f05cfc4097eecf0236d02070aa6c7cb5679
maurapintor <maura.pintor@gmail.com>
2020-02-17 11:13:10 +0100

    feat: fixed loss of elastic net method

--- src/secml/adv/attacks/evasion/cleverhans/c_attack_evasion_cleverhans_losses.py
+++ src/secml/adv/attacks/evasion/cleverhans/c_attack_evasion_cleverhans_losses.py
@@ -86,6 +86,26 @@ class CAttackEvasionCleverhansLossesMixin(object):
 
     def _objective_function_elastic_net(self, x):
 
+        if self._stored_vars is not None and \
+                'const' in self._stored_vars:
+            stored_const = self._stored_vars['const'][0]
+            if self._x0.shape[0] == 1:
+                # use same const for all points
+                c_weight = stored_const.item()
+            else:
+                # each point has it own optimized const
+                c_weight = CArray.empty(shape=(len(stored_const)))
+                for i, c in enumerate(stored_const):
+                    c_weight[i] = c
+        else:
+            self.logger.info('Constant value not stored during run. Using '
+                             'initial_const value. For computing the loss '
+                             'with the actual value of const set '
+                             '`store_var_lst=["const"]` in '
+                             'CAttackEvasionCleverhans.__init__().')
+
+            c_weight = self._clvrh_attack.initial_const
+
         if self._clvrh_attack.decision_rule is 'L1':
             d = ((self._x0 - x).abs()).sum(axis=1).ravel()
         elif self._clvrh_attack.decision_rule is 'L2':
@@ -119,18 +139,6 @@ class CAttackEvasionCleverhansLossesMixin(object):
         # would have the desired effect (loss term is <= 0 if and only if
         # the difference between the logit of the target and any other class
         # differs by at least confidence). Hence the rearrangement here.
-        # FIXME: the value of c_weight should be collected from the
-        #  graph after the attack run as it is optimized through
-        #  a binary search and it is a variable in the tf graph
-        if self._clvrh_attack.binary_search_steps > 1:
-            self.logger.warning(
-                "The objective function computation currently only supports "
-                "`binary_search_steps=1`. The attack is running the "
-                "line search, but the loss function obtained with this "
-                "method is not yet recovering the value of `c` after "
-                "optimization. Using initial value of constant `c` "
-                "for computing the loss.")
-        c_weight = self._clvrh_attack.initial_const
         self.confidence = self._clvrh_attack.confidence
 
         if self.y_target is not None:
