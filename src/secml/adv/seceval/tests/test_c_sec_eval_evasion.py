d7512af493dea3257e6ac0982ec9cafda05c1c0a
Maura Pintor <maura.pintor@gmail.com>
2020-05-08 17:12:52 +0200

    Resolve "Security Evaluation on Cleverhans Attacks"

--- src/secml/adv/seceval/tests/test_c_sec_eval_evasion.py
+++ src/secml/adv/seceval/tests/test_c_sec_eval_evasion.py
@@ -1,37 +1,38 @@
-from secml.testing import CUnitTest
+from cleverhans.attacks import FastGradientMethod
 
-from secml.adv.seceval import CSecEval
+from secml.adv.attacks import CAttackEvasionCleverhans
 from secml.adv.attacks.evasion import CAttackEvasionPGDLS
+from secml.adv.seceval import CSecEval
 from secml.array import CArray
 from secml.data.loader import CDLRandomBlobs
 from secml.figure import CFigure
 from secml.ml.classifiers import CClassifierSVM
+from secml.testing import CUnitTest
 
 
 class TestCSecEval(CUnitTest):
     """Unittests for CSecEval (evasion attack)."""
 
     def setUp(self):
-        
-        classifier = CClassifierSVM(
+
+        self.classifier = CClassifierSVM(
             kernel='linear', C=1.0, grad_sampling=1.0)
 
         # data parameters
-        discrete = False
+        self.discrete = False
 
-        lb = -2
-        ub = +2
+        self.lb = -2
+        self.ub = +2
 
         n_tr = 20
         n_ts = 10
         n_features = 2
-        
+
         n_reps = 1
 
         self.sec_eval = []
         self.attack_ds = []
         for rep_i in range(n_reps):
-
             self.logger.info(
                 "Loading `random_blobs` with seed: {:}".format(rep_i))
             loader = CDLRandomBlobs(
@@ -43,70 +44,95 @@ class TestCSecEval(CUnitTest):
                 random_state=rep_i * 100 + 10)
             ds = loader.load()
 
-            tr = ds[:n_tr, :]
-            ts = ds[n_tr:, :]
-            
-            classifier.fit(tr.X, tr.Y)
-            
-            self.attack_ds.append(ts)
+            self.tr = ds[:n_tr, :]
+            self.ts = ds[n_tr:, :]
+
+            self.classifier.fit(self.tr.X, self.tr.Y)
 
             # only manipulate positive samples, targeting negative ones
             self.y_target = None
-            attack_classes = CArray([1])
-        
-            params = {
-                "classifier": classifier,
-                "surrogate_classifier": classifier,
-                "surrogate_data": tr,
-                "distance": 'l1',
-                "lb": lb,
-                "ub": ub,
-                "discrete": discrete,
-                "y_target": self.y_target,
-                "attack_classes": attack_classes,
-                "solver_params": {'eta': 0.5, 'eps': 1e-2}
-            }
-            attack = CAttackEvasionPGDLS(**params)
-            attack.verbose = 1
-        
-            # sec eval params
-            param_name = 'dmax'
-            dmax = 2
-            dmax_step = 0.5
-            param_values = CArray.arange(
-                start=0, step=dmax_step,
-                stop=dmax + dmax_step)
+            self.attack_classes = CArray([1])
 
-            # set sec eval object
-            self.sec_eval.append(
-                CSecEval(
-                    attack=attack,
-                    param_name=param_name,
-                    param_values=param_values,
+            for create_fn in (self._attack_pgd_ls, self._attack_cleverhans):
+                self.attack_ds.append(self.ts)
+                attack, param_name, param_values = create_fn()
+                # set sec eval object
+                self.sec_eval.append(
+                    CSecEval(
+                        attack=attack,
+                        param_name=param_name,
+                        param_values=param_values,
                     )
-            )
+                )
+
+    def _attack_pgd_ls(self):
+        params = {
+            "classifier": self.classifier,
+            "surrogate_classifier": self.classifier,
+            "surrogate_data": self.tr,
+            "distance": 'l1',
+            "lb": self.lb,
+            "ub": self.ub,
+            "discrete": self.discrete,
+            "y_target": self.y_target,
+            "attack_classes": self.attack_classes,
+            "solver_params": {'eta': 0.5, 'eps': 1e-2}
+        }
+        attack = CAttackEvasionPGDLS(**params)
+        attack.verbose = 1
+
+        # sec eval params
+        param_name = 'dmax'
+        dmax = 2
+        dmax_step = 0.5
+        param_values = CArray.arange(
+            start=0, step=dmax_step,
+            stop=dmax + dmax_step)
+
+        return attack, param_name, param_values
+
+    def _attack_cleverhans(self):
+        attack_params = {'eps': 0.1,
+                         'clip_max': self.ub,
+                         'clip_min': self.lb,
+                         'ord': 1}
+
+        attack = CAttackEvasionCleverhans(
+            classifier = self.classifier,
+            surrogate_classifier = self.classifier,
+            surrogate_data = self.tr,
+            y_target = self.y_target,
+            clvh_attack_class = FastGradientMethod,
+            ** attack_params)
+
+        param_name = 'attack_params.eps'
+        dmax = 2
+        dmax_step = 0.5
+        param_values = CArray.arange(
+            start=0, step=dmax_step,
+            stop=dmax + dmax_step)
+
+        return attack, param_name, param_values
 
     def _plot_sec_eval(self):
         # figure creation
         figure = CFigure(height=5, width=5)
 
-        sec_eval_data = [
-            sec_eval.sec_eval_data for sec_eval in self.sec_eval]
-        # plot security evaluation
-        figure.sp.plot_sec_eval(sec_eval_data, label='SVM', marker='o',
-                                show_average=True, mean=True)
+        for sec_eval in self.sec_eval:
+            sec_eval_data = sec_eval.sec_eval_data
+            # plot security evaluation
+            figure.sp.plot_sec_eval(sec_eval_data, label='SVM', marker='o',
+                                    show_average=True, mean=True)
 
         figure.subplots_adjust()
         figure.show()
 
     def test_sec_eval(self):
-
         # evaluate classifier security
         for sec_eval_i, sec_eval in enumerate(self.sec_eval):
             sec_eval.run_sec_eval(self.attack_ds[sec_eval_i])
 
         self._plot_sec_eval()
 
-
-if __name__ == '__main__':
-    CUnitTest.main()
+    if __name__ == '__main__':
+        CUnitTest.main()
