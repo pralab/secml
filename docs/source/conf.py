14e3fbdc3cb36f0fcf7e6e0f5b0cf48a4f236517
Marco Melis <marco.melis@unica.it>
2021-02-08 11:08:35 +0100

    #899 Added "Edit on Gitlab" button to doc pages.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,6 +121,16 @@ html_theme_options = {
     'titles_only': False
 }
 
+html_context = {
+    "display_gitlab": True,
+    "gitlab_host": "gitlab.com",
+    "gitlab_user": "secml",
+    "gitlab_repo": 'secml',
+    "gitlab_version": "HEAD",
+    "conf_py_path": "/docs/source/",
+    "source_suffix": '.rst'
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
