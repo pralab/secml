ca0bc86736baf76dca34e56249364b40f719594f
Marco Melis <marco.melis@unica.it>
2021-04-12 10:18:37 +0200

    Updated unittests for CConstraintL1 to cover #919 and #920.

--- src/secml/optim/constraints/tests/test_c_constraint_l1.py
+++ src/secml/optim/constraints/tests/test_c_constraint_l1.py
@@ -11,79 +11,128 @@ class TestCConstraintL1(CConstraintTestCases):
 
     def setUp(self):
 
+        self.c0 = CConstraintL1()  # center=0, radius=1
         self.c = CConstraintL1(center=1, radius=1)
         self.c_array = CConstraintL1(center=CArray([1, 1]), radius=1)
 
-        # create a point that lies inside the constraint
-        self.p1_inside = CArray([1., 1.])
-        # create a point that lies outside the constraint
-        self.p2_outside = CArray([2., 2.])
-        # create a point that lies on the constraint
-        self.p3_on = CArray([0., 1.])
+        # create a point that lies inside the constraints
+        self.c0_p1_inside = CArray([0., 0.])
+        self.c_p1_inside = CArray([1., 1.])
+        # create a point that lies outside the constraints
+        self.c0_p2_outside = CArray([1., 1.])
+        self.c_p2_outside = CArray([2., 2.])
+        # create a point that lies on the constraints
+        self.c0_p3_on = CArray([0., 1.])
+        self.c_p3_on = CArray([0., 1.])
 
     def test_is_active(self):
         """Test for CConstraint.is_active()."""
         self._test_is_active(
-            self.c, self.p1_inside, self.p2_outside, self.p3_on)
+            self.c0, self.c0_p1_inside, self.c0_p2_outside, self.c0_p3_on)
         self._test_is_active(
-            self.c_array, self.p1_inside, self.p2_outside, self.p3_on)
+            self.c, self.c_p1_inside, self.c_p2_outside, self.c_p3_on)
+        self._test_is_active(
+            self.c_array, self.c_p1_inside, self.c_p2_outside, self.c_p3_on)
 
-        # Test for sparse arrays, works only for a center defined as CArray
+        # Test for sparse arrays
         self._test_is_violated(
-            self.c_array, self.p1_inside.tosparse(),
-            self.p2_outside.tosparse(), self.p3_on.tosparse())
+            self.c0,
+            self.c0_p1_inside.tosparse(),
+            self.c0_p2_outside.tosparse(),
+            self.c0_p3_on.tosparse())
+        self._test_is_violated(
+            self.c_array,
+            self.c_p1_inside.tosparse(),
+            self.c_p2_outside.tosparse(),
+            self.c_p3_on.tosparse())
 
     def test_is_violated(self):
         """Test for CConstraint.is_violated()."""
         self._test_is_violated(
-            self.c, self.p1_inside, self.p2_outside, self.p3_on)
+            self.c0, self.c0_p1_inside, self.c0_p2_outside, self.c0_p3_on)
         self._test_is_violated(
-            self.c_array, self.p1_inside, self.p2_outside, self.p3_on)
+            self.c, self.c_p1_inside, self.c_p2_outside, self.c_p3_on)
+        self._test_is_violated(
+            self.c_array, self.c_p1_inside, self.c_p2_outside, self.c_p3_on)
 
-        # Test for sparse arrays, works only for a center defined as CArray
+        # Test for sparse arrays
         self._test_is_active(
-            self.c_array, self.p1_inside.tosparse(),
-            self.p2_outside.tosparse(), self.p3_on.tosparse())
+            self.c0,
+            self.c0_p1_inside.tosparse(),
+            self.c0_p2_outside.tosparse(),
+            self.c0_p3_on.tosparse())
+        self._test_is_active(
+            self.c_array,
+            self.c_p1_inside.tosparse(),
+            self.c_p2_outside.tosparse(),
+            self.c_p3_on.tosparse())
 
     def test_constraint(self):
         """Test for CConstraint.constraint()."""
         self._test_constraint(
-            self.c, self.p1_inside, self.p2_outside, self.p3_on)
+            self.c0, self.c0_p1_inside, self.c0_p2_outside, self.c0_p3_on)
         self._test_constraint(
-            self.c_array, self.p1_inside, self.p2_outside, self.p3_on)
+            self.c, self.c_p1_inside, self.c_p2_outside, self.c_p3_on)
+        self._test_constraint(
+            self.c_array, self.c_p1_inside, self.c_p2_outside, self.c_p3_on)
 
-        # Test for sparse arrays, works only for a center defined as CArray
+        # Test for sparse arrays
         self._test_constraint(
-            self.c_array, self.p1_inside.tosparse(),
-            self.p2_outside.tosparse(), self.p3_on.tosparse())
+            self.c0,
+            self.c0_p1_inside.tosparse(),
+            self.c0_p2_outside.tosparse(),
+            self.c0_p3_on.tosparse())
+        self._test_constraint(
+            self.c_array,
+            self.c_p1_inside.tosparse(),
+            self.c_p2_outside.tosparse(),
+            self.c_p3_on.tosparse())
 
     def test_projection(self):
         """Test for CConstraint.projection()."""
-        self._test_projection(self.c, self.p1_inside, self.p2_outside,
-                              self.p3_on, CArray([1.5, 1.5]))
-        self._test_projection(self.c_array, self.p1_inside, self.p2_outside,
-                              self.p3_on, CArray([1.5, 1.5]))
+        self._test_projection(self.c0, self.c0_p1_inside, self.c0_p2_outside,
+                              self.c0_p3_on, CArray([0.5, 0.5]))
+        self._test_projection(self.c, self.c_p1_inside, self.c_p2_outside,
+                              self.c_p3_on, CArray([1.5, 1.5]))
+        self._test_projection(self.c_array, self.c_p1_inside, self.c_p2_outside,
+                              self.c_p3_on, CArray([1.5, 1.5]))
 
-        # Test for sparse arrays, works only for a center defined as CArray
+        # Test for sparse arrays
         self._test_projection(
-            self.c_array, self.p1_inside.tosparse(),
-            self.p2_outside.tosparse(), self.p3_on.tosparse(),
+            self.c0, self.c0_p1_inside.tosparse(),
+            self.c0_p2_outside.tosparse(), self.c0_p3_on.tosparse(),
+            CArray([0.5, 0.5], tosparse=True))
+        self._test_projection(
+            self.c_array, self.c_p1_inside.tosparse(),
+            self.c_p2_outside.tosparse(), self.c_p3_on.tosparse(),
             CArray([1.5, 1.5], tosparse=True))
 
     def test_gradient(self):
         """Test for CConstraint.gradient()."""
-        # [1. 1.] is the center of the constraint, expected grad [0, c0]
-        # however, numerical gradient is struggling so we avoid its omparison
+        # [0. 0.] is the center of the constraint, expected grad [0, c0]
+        # however, numerical gradient is struggling so we avoid its comparison
         self.assert_array_almost_equal(
-            self.c.gradient(self.p1_inside), CArray([0, 0]))
+            self.c0.gradient(self.c0_p1_inside), CArray([0, 0]))
+
+        # [1. 1.] is the center of the constraint, expected grad [0, c0]
+        # however, numerical gradient is struggling so we avoid its comparison
+        self.assert_array_almost_equal(
+            self.c.gradient(self.c_p1_inside), CArray([0, 0]))
+
+        self._test_gradient(self.c0, CArray([0.1, 0.2]))
+        self._test_gradient(self.c0, self.c0_p2_outside)
 
         self._test_gradient(self.c, CArray([1.1, 1.2]))
-        self._test_gradient(self.c, self.p2_outside)
+        self._test_gradient(self.c, self.c_p2_outside)
 
+        # [0. 1.] is the verge of the constraint, expected grad [0, 1]
+        # however, numerical gradient is struggling so we avoid its comparison
+        self.assert_array_almost_equal(
+            self.c0.gradient(self.c0_p3_on), CArray([0., 1.]))
         # [0. 1.] is the verge of the constraint, expected grad [-1, 1]
         # however, numerical gradient is struggling so we avoid its comparison
         self.assert_array_almost_equal(
-            self.c.gradient(self.p3_on), CArray([-1., 0.]))
+            self.c.gradient(self.c_p3_on), CArray([-1., 0.]))
 
     def test_subgradient(self):
         """Check if the subgradient is computed correctly
@@ -117,8 +166,16 @@ class TestCConstraintL1(CConstraintTestCases):
     def test_plot(self):
         """Visualize the constraint."""
         # Plotting constraint and "critical" points
-        self._test_plot(
-            self.c, self.p1_inside, self.p2_outside, self.p3_on)
+        self._test_plot(self.c0,
+                        self.c0_p1_inside,
+                        self.c0_p2_outside,
+                        self.c0_p3_on,
+                        label='c0')
+        self._test_plot(self.c,
+                        self.c_p1_inside,
+                        self.c_p2_outside,
+                        self.c_p3_on,
+                        label='c')
 
 
 if __name__ == '__main__':
